c0ac429f6d4a97f2b32af5cf4c4ba29b0d23db2b
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-03-27 11:34:04 +0200

    Add redux support and persistence

--- src/index.js
+++ src/index.js
@@ -1,8 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Provider} from 'react-redux'
+import {ConnectedRouter} from 'react-router-redux'
+import {Route, Switch} from 'react-router-dom'
+import createHistory from 'history/createBrowserHistory'
+import {PersistGate} from 'redux-persist/es/integration/react'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+import configureStore from 'store'
+
+const history = createHistory()
+const {store, persistor} = configureStore(history)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <PersistGate loading={<div/>} persistor={persistor}>
+        <Switch>
+          <Route path='/' component={App}/>
+        </Switch>
+      </PersistGate>
+    </ConnectedRouter>
+  </Provider>, document.getElementById('root'))
+registerServiceWorker()
