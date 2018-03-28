c243df70c6e820b8fdda09bcf88bcab57f8f188f
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-03-28 09:24:22 +0200

    Add Sidebar

--- src/containers/main/MainPage/MainPageContainer.js
+++ src/containers/main/MainPage/MainPageContainer.js
@@ -1,16 +1,27 @@
 import { connect } from 'react-redux'
 import { isAuthenticated } from 'reducers/auth'
 import {withRouter} from 'react-router-dom'
-import MainPage from 'pages/main/MainPage/MainPage'
 
-function mapStateToProps ({auth}) {
+import MainPage from 'pages/main/MainPage/MainPage'
+import * as sidebarActionCreators from 'actions/sidebar'
+import {bindActionCreators} from 'redux'
+
+function mapStateToProps ({auth, sidebar}) {
   return {
     errors: auth.errors,
     isAuthenticated: isAuthenticated(auth),
-    loginInProgress: auth.loginInProgress
+    loginInProgress: auth.loginInProgress,
+    isVisible: sidebar.isVisible,
+    contentDimmed: sidebar.contentDimmed,
+    hideSidebar: sidebar.hideSidebar
   }
 }
 
+function mapDispatchToProps (dispatch) {
+  return bindActionCreators(sidebarActionCreators, dispatch)
+}
+
 export default withRouter(connect(
-  mapStateToProps
+  mapStateToProps,
+  mapDispatchToProps
 )(MainPage))
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,10 +2,12 @@ import {routerReducer} from 'react-router-redux/reducer'
 import {combineReducers} from 'redux'
 import auth from './auth'
 import user from './user'
+import sidebar from './sidebar'
 
 const rootReducer = combineReducers({
   auth,
   user,
+  sidebar,
   router: routerReducer
 })
 
