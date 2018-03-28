74707187729a13daf4f23e6df9cd23fcc624d04c
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-03-28 15:15:09 +0200

    Remove unused props

--- src/containers/main/calls/CallPage/CallPageContainer.js
+++ src/containers/main/calls/CallPage/CallPageContainer.js
@@ -1,16 +1,6 @@
-import { bindActionCreators } from 'redux'
-
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 import {CallPage} from 'pages/main/calls'
-
-function mapStateToProps ({calls}) {
-  return {}
-}
-
-function mapDispatchToProps (dispatch) {
-  return bindActionCreators({}, dispatch)
-}
 
 export default withRouter(connect(
   null,
--- src/containers/main/chat/ChatPage/ChatPageContainer.js
+++ src/containers/main/chat/ChatPage/ChatPageContainer.js
@@ -1,16 +1,7 @@
-import { bindActionCreators } from 'redux'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
 
 import {ChatPage} from 'pages/main/chat'
-
-function mapStateToProps ({calls}) {
-  return {}
-}
-
-function mapDispatchToProps (dispatch) {
-  return bindActionCreators({}, dispatch)
-}
 
 export default withRouter(connect(
   null,
