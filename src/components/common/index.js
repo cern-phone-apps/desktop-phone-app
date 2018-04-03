d9cba325672046afc83a778de19a6e574f5cec2e
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-04-03 12:14:38 +0200

    Add Scrollable component

--- src/components/common/index.js
+++ src/components/common/index.js
@@ -1,3 +1,4 @@
 export {default as LeftColumn} from './LeftColumn/LeftColumn'
 export {default as RightColumn} from './RightColumn/RightColumn'
 export {default as LeftColumnHeader} from './LeftColumnHeader/LeftColumnHeader'
+export {default as ScrollableContent} from './ScrollableContent/ScrollableContent'
