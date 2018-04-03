907e831c403b1ba435d43bdf1def9a4c445b4b86
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-04-03 12:15:03 +0200

    Change styles to ToggleButton

--- src/components/sidebar/ToggleButton/ToggleButton.js
+++ src/components/sidebar/ToggleButton/ToggleButton.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react'
 import PropTypes from 'prop-types'
 import {Button} from 'semantic-ui-react'
 
+import './ToggleButton.css'
+
 class ToggleButton extends Component {
   static propTypes = {
     displaySidebar: PropTypes.func.isRequired,
@@ -19,7 +21,7 @@ class ToggleButton extends Component {
 
   render () {
     return (
-      <Button as={'a'} className={'flat'} icon={'sidebar'} onClick={this.toggleSidebar}/>
+      <Button as={'a'} className={'flat ToggleButton'} icon={'sidebar'} onClick={this.toggleSidebar}/>
     )
   }
 }
