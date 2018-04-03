0cff52656fe07de40b9cb0c7388c192918e6bc4b
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-04-03 16:25:10 +0200

    Add OnCallDetails component

--- src/components/calls/index.js
+++ src/components/calls/index.js
@@ -1,2 +1,6 @@
 export {default as RecentCallsList} from './RecentCallsList/RecentCallsList'
 export {default as StatusSwitcher} from './StatusSwitcher/StatusSwitcher'
+export {default as OnCallMessage} from './OnCallMessage/OnCallMessage'
+export {default as CallLoader} from './CallLoader/CallLoader'
+export {default as PhoneRingingIcon} from './PhoneRingingIcon/PhoneRingingIcon'
+export {default as OnCallDetails} from './OnCallDetails/OnCallDetails'
