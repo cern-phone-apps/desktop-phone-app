1b3b401745fdca9376e5c67a5afed7c9cbe43d95
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-05-14 10:49:45 +0200

    Regorganize Sidebar

--- src/containers/settings/index.js
+++
@@ -1,3 +0,0 @@
-export {default as ModalSettingsContainer} from './ModalSettings/ModalSettingsContainer'
-export {default as PersonalInfoContainer} from './PersonalInfo/PersonalInfoContainer'
-export {default as DeviceSettingsContainer} from './DeviceSettings/DeviceSettingsContainer'
