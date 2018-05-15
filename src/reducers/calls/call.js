5d2c939365463fdd9b5744ac942825900cd422bb
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-05-15 16:05:05 +0200

    Refactor call reducer

--- src/reducers/calls/call.js
+++ src/reducers/calls/call.js
@@ -12,50 +12,74 @@ const initialState = {
 }
 
 const call = (state = initialState, action) => {
+  function processCall () {
+    return {
+      ...state,
+      calling: true,
+      onCall: false,
+      recipient: action.recipient
+    }
+  }
+
+  function processCallRejected () {
+    return {
+      ...state,
+      onCall: false,
+      calling: false
+    }
+  }
+
+  function processCallMissed () {
+    return {
+      ...state,
+      onCall: false,
+      calling: false
+    }
+  }
+
+  function processCallReceiving () {
+    return {
+      ...state,
+      onCall: false,
+      receivingCall: true
+    }
+  }
+
+  function processCallHangup () {
+    return {
+      ...state,
+      onCall: false,
+      calling: false,
+      recipient: {}
+    }
+  }
+
+  function processCallAccepted () {
+    return {
+      ...state,
+      onCall: true,
+      calling: false,
+      receivingCall: false
+    }
+  }
+
   switch (action.type) {
     case callActions.CALL:
-      return {
-        ...state,
-        calling: true,
-        onCall: false,
-        recipient: action.recipient
-      }
+      return processCall()
     case callActions.CALL_ACCEPTED:
       if (state.calling === true) {
-        return {
-          ...state,
-          onCall: true,
-          calling: false,
-          receivingCall: false
-        }
+        return processCallAccepted()
       } else {
         return state
       }
     case callActions.CALL_REJECTED:
-      return {
-        ...state,
-        onCall: false,
-        calling: false
-      }
+      return processCallRejected()
     case callActions.CALL_MISSED:
-      return {
-        ...state,
-        onCall: false,
-        calling: false
-      }
+      return processCallMissed()
     case callActions.IS_RECEIVING_CALL:
-      return {
-        ...state,
-        onCall: false,
-        receivingCall: true
-      }
+      return processCallReceiving()
     case callActions.HANGUP_CALL:
-      return {
-        ...state,
-        onCall: false,
-        calling: false,
-        recipient: {}
-      }
+      return processCallHangup()
 
     default:
       return state
