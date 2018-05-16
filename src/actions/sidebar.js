d3396a724ddd7f945a0338fd67a975f09d8f1845
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-05-16 12:29:53 +0200

    Remove comments from sidebar actions

--- src/actions/sidebar.js
+++ src/actions/sidebar.js
@@ -4,28 +4,24 @@ export const FINISHED_DISPLAYING = '@@sidebar/FINISHED_DISPLAYING'
 export const HIDE_SIDEBAR = '@@sidebar/HIDE_SIDEBAR'
 
 export function displaySidebar () {
-  console.debug('dispatching displaySidebar')
   return {
     type: DISPLAY_SIDEBAR
   }
 }
 
 export function displayingSidebar () {
-  console.debug('dispatching displaySidebar')
   return {
     type: IS_DISPLAYING
   }
 }
 
 export function finishedDisplayingSidebar () {
-  console.debug('dispatching finishedDisplayingSidebar')
   return {
     type: FINISHED_DISPLAYING
   }
 }
 
 export function hideSidebar () {
-  console.debug('dispatching hideSidebar')
   return {
     type: HIDE_SIDEBAR
   }
