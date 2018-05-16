9480d6fcf4b96884075ec051e282a24b68721c7b
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-05-16 12:17:47 +0200

    Add mocks for rsaa calls and status tests

---
+++ src/actions/calls/status.test.js
@@ -0,0 +1,24 @@
+import * as actions from 'actions/calls/status'
+
+describe('status actions', () => {
+  it('should create an action to set user available', () => {
+    const expectedAction = {
+      type: actions.SET_AVAILABLE
+    }
+    expect(actions.setUserAvailable()).toEqual(expectedAction)
+  })
+
+  it('should create an action to set user invisible', () => {
+    const expectedAction = {
+      type: actions.SET_INVISIBLE
+    }
+    expect(actions.setUserInvisible()).toEqual(expectedAction)
+  })
+
+  it('should create an action to set user available', () => {
+    const expectedAction = {
+      type: actions.SET_DO_NOT_DISTURB
+    }
+    expect(actions.setUserDoNotDisturb()).toEqual(expectedAction)
+  })
+})
