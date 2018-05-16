09f5754add12ab2666b020c18cb3ce05b7735d46
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-05-16 12:10:49 +0200

    Add tests for rsaa search request

--- src/actions/calls/search.test.js
+++ src/actions/calls/search.test.js
@@ -1,4 +1,12 @@
+import configureMockStore from 'redux-mock-store'
+import {apiMiddleware} from 'redux-api-middleware'
+import thunk from 'redux-thunk'
+import fetchMock from 'fetch-mock'
+
 import * as actions from 'actions/calls/search'
+
+const middlewares = [thunk, apiMiddleware]
+const mockStore = configureMockStore(middlewares)
 
 describe('search actions', () => {
   it('should create an action to select user', () => {
@@ -28,5 +36,47 @@ describe('search actions', () => {
     const value = 'USERNAME'
     const expectedResult = `https://hostname/api/users/?username=${value}`
     expect(actions.buildSearchEndpoint(value)).toEqual(expectedResult)
+  })
+
+  // it('should make a request to the users search api', async () => {
+  //   const value = 'USERNAME'
+  //   nock('https://hostname')
+  //     .get(`/api/users/?username=${value}`)
+  //     .reply(200, {payload: 'OK!'})
+  //
+  //   const expectedActions = [
+  //     { type: 'SEARCH_REQUEST', payload: undefined, meta: undefined },
+  //     { type: 'SEARCH_SUCCESS', payload: { payload: 'OK!' }, meta: undefined },
+  //     { type: 'SEARCH_FAILURE', payload: { payload: 'OK!' }, meta: undefined }
+  //   ]
+  //
+  //   const store = createMockStore({})
+  //   store.dispatch(actions.searchUsers(value))
+  //
+  //   // const expectedResult = `https://hostname/api/users/?username=${value}`
+  //   // expect(actions.searchUsers(value)).toEqual(expectedResult)
+  // })
+})
+
+describe('redux-api-middleware test', () => {
+  afterEach(() => {
+    fetchMock.reset()
+    fetchMock.restore()
+  })
+
+  it('should dispatch xxx when myActionCreator being called', () => {
+    const value = 'USERNAME'
+    const body = [{division: 'DIVISION', cernGroup: 'GROUP', cernSection: 'SECTION', displayName: 'NAME'}]
+    fetchMock.getOnce(`https://hostname/api/users/?username=${value}`,
+      { body: body, headers: { 'content-type': 'application/json' } })
+    const expectedActions = [
+      { type: actions.SEARCH_REQUEST },
+      { type: actions.SEARCH_SUCCESS, payload: body }
+    ]
+    const store = mockStore({})
+    return store.dispatch(actions.searchUsers(value)).then(() => {
+      // return of async actions
+      expect(store.getActions()).toEqual(expectedActions)
+    })
   })
 })
