b6ada4b646e053d762038a3080e51e60b20d7b16
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-05-17 12:03:18 +0200

    Add more tests for auth rsaa and sidebar

--- src/actions/auth.test.js
+++ src/actions/auth.test.js
@@ -31,4 +31,38 @@ describe('async auth actions', () => {
       expect(store.getActions()).toEqual(expectedActions)
     })
   })
+
+  it('should dispatch LOGOUT_SUCCESS when logout is called', () => {
+    const body = {
+      logout: true
+    }
+    fetchMock.deleteOnce(`https://hostname/api/auth/logout/`,
+      {body: body, headers: {'content-type': 'application/json'}})
+    const expectedActions = [
+      {type: actions.LOGOUT_REQUEST},
+      {type: actions.LOGOUT_SUCCESS, payload: body}
+    ]
+    const store = mockStore({})
+    return store.dispatch(actions.logout()).then(() => {
+      // return of async actions
+      expect(store.getActions()).toEqual(expectedActions)
+    })
+  })
+
+  it('should dispatch LOGOUT_SUCCESS when logout is called', () => {
+    const body = {
+      refresh: true
+    }
+    fetchMock.postOnce(`https://hostname/api/auth/refresh/`,
+      {body: body, headers: {'content-type': 'application/json'}})
+    const expectedActions = [
+      {type: actions.TOKEN_REQUEST},
+      {type: actions.TOKEN_RECEIVED, payload: body}
+    ]
+    const store = mockStore({})
+    return store.dispatch(actions.refreshAccessToken()).then(() => {
+      // return of async actions
+      expect(store.getActions()).toEqual(expectedActions)
+    })
+  })
 })
