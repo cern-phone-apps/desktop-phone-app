3a433c6ccd102b4e4209def84b5e8a9c2ad8df88
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-05-17 12:03:18 +0200

    Add more tests for auth rsaa and sidebar

---
+++ src/actions/sidebar.test.js
@@ -0,0 +1,32 @@
+import * as actions from 'actions/sidebar'
+
+describe('sidebar actions', () => {
+  it('should create an action to display sidebar', () => {
+    const expectedAction = {
+      type: actions.DISPLAY_SIDEBAR
+    }
+    expect(actions.displaySidebar()).toEqual(expectedAction)
+  })
+
+  it('should create an action to hide the sidebar', () => {
+    const expectedAction = {
+      type: actions.HIDE_SIDEBAR
+    }
+    expect(actions.hideSidebar()).toEqual(expectedAction)
+  })
+
+  it('should create an action when is displaying', () => {
+    const expectedAction = {
+      type: actions.IS_DISPLAYING
+    }
+    expect(actions.displayingSidebar()).toEqual(expectedAction)
+  })
+
+  it('should create an action when is hiding', () => {
+    const expectedAction = {
+      type: actions.FINISHED_DISPLAYING
+    }
+    expect(actions.finishedDisplayingSidebar()).toEqual(expectedAction)
+  })
+
+})
