84ed12cc50860034d4a73c7df92f105d78be8e13
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-05-17 11:18:14 +0200

    Add tests for devices actions

---
+++ src/actions/settings/devices.test.js
@@ -0,0 +1,22 @@
+import * as actions from 'actions/settings/devices'
+
+describe('devices actions', () => {
+  it('should create an action to set the microphone', () => {
+    const deviceId = '123456'
+    const expectedAction = {
+      type: actions.SET_MICROPHONE,
+      deviceId
+    }
+    expect(actions.setMicrophone(deviceId)).toEqual(expectedAction)
+  })
+
+  it('should create an action to set the speaker', () => {
+    const deviceId = '123456'
+    const expectedAction = {
+      type: actions.SET_SPEAKER,
+      deviceId
+    }
+    expect(actions.setSpeaker(deviceId)).toEqual(expectedAction)
+  })
+
+})
