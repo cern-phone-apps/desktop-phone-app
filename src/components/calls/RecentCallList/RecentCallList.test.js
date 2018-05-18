033bb3855fd3cd6c882cc10429b8db2728ed8702
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-05-18 09:32:39 +0200

    Update RecentCallList tests

--- src/components/calls/RecentCallList/RecentCallList.test.js
+++ src/components/calls/RecentCallList/RecentCallList.test.js
@@ -1,14 +1,10 @@
 import React from 'react'
 import {shallow} from 'enzyme'
 import RecentCallList from './RecentCallList'
-import RecentCall from 'components/calls/RecentCall/RecentCall'
 
 describe('RecentCallList tests', () => {
   const mockFn = jest.fn()
   it('renders without crashing', () => {
     const wrapper = shallow(<RecentCallList calling={false} makeCall={mockFn} onCall={false}/>)
-    // No RecentCall items (empty list)
-    // expect(wrapper.text()).not.toEqual(expect.stringContaining('RecentCall'))
-
   })
 })
