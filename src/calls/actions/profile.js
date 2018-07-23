b120cb74c14d179eeb79c70df4143303c1a55f35
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-07-23 11:58:38 +0200

    Add profile api call and reducer

---
+++ src/calls/actions/profile.js
@@ -0,0 +1,30 @@
+import { RSAA } from 'redux-api-middleware'
+import { withAuth } from 'login/reducers/auth'
+import {buildCallsApiEndpoint} from 'calls/actions/numbers'
+
+export const PROFILE_REQUEST = '@@search/PROFILE_REQUEST'
+export const PROFILE_SUCCESS = '@@search/PROFILE_SUCCESS'
+export const PROFILE_FAILURE = '@@search/PROFILE_FAILURE'
+
+/**
+ * Creates an endpoint URL for the user search
+ *
+ * @param name Name or surname of the user to search
+ * @returns {string} A URL for the request
+ */
+export function buildProfileEndpoint (name) {
+  return `${buildCallsApiEndpoint(process.env.REACT_APP_API_USER_PROFILE_PATH)}?username=${name}`
+}
+
+
+export const getUserProfile = (name) => ({
+  [RSAA]: {
+    endpoint: buildProfileEndpoint(name),
+    method: 'GET',
+    credentials: 'include',
+    headers: withAuth({'Content-Type': 'application/json'}),
+    types: [
+      PROFILE_REQUEST, PROFILE_SUCCESS, PROFILE_FAILURE
+    ]
+  }
+})
