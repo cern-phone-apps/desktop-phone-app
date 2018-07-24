8621f03dabb0f4a624ac81595e0e304531efc791
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-07-24 08:58:54 +0200

    Update tests for LoadingDimmer

---
+++ src/__tests__/login/components/LoadingDimmer/LoadingDimmer.test.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import 'i18n'
+import {LoadingDimmer} from 'login/components/LoadingDimmer/LoadingDimmer'
+import {Dimmer, Loader} from 'semantic-ui-react'
+
+
+it('renders without crashing', () => {
+  const loader = shallow(<LoadingDimmer t={key => key}/>);
+
+  expect(loader.text()).toEqual('<Dimmer />');
+  expect(loader.html()).toContain('loadingText');
+  expect(loader.html()).toContain('visible');
+  expect(loader.html()).toContain('inverted');
+})
+
+
+it('renders without crashing', () => {
+  const loader = shallow(<LoadingDimmer t={key => key}/>);
+
+  expect(loader.find(Dimmer)).toHaveLength(1);
+  expect(loader.find(Loader)).toHaveLength(1);
+})
