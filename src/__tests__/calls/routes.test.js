4a78788eaba979a82d9264d5b2888229b5161402
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-07-26 13:47:29 +0200

    Add tests for calls routes

---
+++ src/__tests__/calls/routes.test.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {callsRoute, callsMain, callsRoutes} from 'calls/routes'
+
+it('has expected path', () => {
+  expect(callsRoute.path).toEqual("/")
+  expect(callsRoute.exact).toEqual(true)
+})
+
+it('renders a Route', () => {
+  const callsComponent = callsMain()
+  const wrapper = shallow(callsComponent)
+  expect(wrapper).toHaveLength(1)
+  expect(wrapper.find("Route")).toHaveLength(1)
+})
+
+it('renders callsRoutes without crashing', () => {
+  const t = jest.fn()
+  const callsComponent = callsRoutes(t)
+  expect(callsComponent).toHaveLength(1)
+})
