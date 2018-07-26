5464321a2b8a3b858199f00c30f300c9dada48a8
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-07-26 11:19:28 +0200

    Add tests for LeftColumnd and ToggleButton

---
+++ src/__tests__/common/components/ToggleButton/ToggleButton.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ToggleButton from 'common/components/ToggleButton/ToggleButton'
+
+
+it('renders without crashing', () => {
+  const displaySidebar = jest.fn()
+  const finishedDisplaySidebar = jest.fn()
+  const wrapper = shallow(<ToggleButton
+    displaySidebar={displaySidebar}
+    finishedDisplayingSidebar={finishedDisplaySidebar}
+    displayTime={500}/>)
+
+  expect(wrapper.text()).toEqual('<Button />')
+  expect(wrapper.html()).toContain('ToggleButton')
+  expect(wrapper.html()).toContain('sidebar')
+})
