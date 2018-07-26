e1a4cf1ffd3bbec34207e1fb64c207aab24a80ac
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-07-26 16:30:10 +0200

    Add test for LeftColumn

---
+++ src/__tests__/common/components/LeftColumnHeader/LeftColumnHeader.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import LeftColumnHeader from 'common/components/LeftColumnHeader/LeftColumnHeader'
+
+
+it('renders without crashing', () => {
+  const wrapper = shallow(<LeftColumnHeader />)
+  expect(wrapper.text()).toEqual('<Grid />')
+  expect(wrapper.debug()).toContain('LeftColumnHeader')
+  expect(wrapper.debug()).toContain('header')
+})
