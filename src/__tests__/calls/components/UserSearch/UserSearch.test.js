88dee04b561223b012f6022a39618a712c1817a4
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-07-27 09:36:05 +0200

    Refactor DeviceSettings component

--- src/__tests__/calls/components/UserSearch/UserSearch.test.js
+++ src/__tests__/calls/components/UserSearch/UserSearch.test.js
@@ -22,3 +22,27 @@ it('renders without crashing', () => {
   expect(wrapper.html()).toContain('results');
   expect(wrapper.html()).toContain('No results found');
 })
+
+
+it('mounts without crashing', () => {
+
+  const searchUsers = jest.fn()
+  const selectUser = jest.fn()
+  const unSelectUser = jest.fn()
+  const updateSearchValue = jest.fn()
+
+  const wrapper = mount(<UserSearch
+    displayDialpad={false}
+    results={[]}
+    searchUsers={searchUsers}
+    selectUser={selectUser}
+    unSelectUser={unSelectUser}
+    updateSearchValue={updateSearchValue}
+    userSelected={false}/>);
+
+  expect(wrapper.text()).toEqual('No results found.');
+  expect(wrapper.html()).toContain('search');
+  expect(wrapper.html()).toContain('input');
+  expect(wrapper.html()).toContain('results');
+  expect(wrapper.html()).toContain('No results found');
+})
