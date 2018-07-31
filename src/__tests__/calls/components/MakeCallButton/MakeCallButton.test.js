056e1f2f5d09787293b3ed292e1031650d53f5d4
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-07-31 11:19:53 +0200

    Add test to MakeCallButton

---
+++ src/__tests__/calls/components/MakeCallButton/MakeCallButton.test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import MakeCallButton from 'calls/components/MakeCallButton/MakeCallButton'
+
+describe('MakeCallButton component', () => {
+  it('renders without crashing', () => {
+    const makeCall = jest.fn()
+    const wrapper = shallow(<MakeCallButton  author={'author name'} calling={false} connected={true} makeCall={makeCall} onCall={false} phoneNumber={'12345'}/>);
+
+    expect(wrapper.text()).toEqual('<Button />');
+    expect(wrapper.debug()).toContain('Button');
+    expect(wrapper.debug()).toContain('phone');
+    expect(wrapper.debug()).toContain('icon');
+  })
+})
