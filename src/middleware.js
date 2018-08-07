22e1d1ba34016d6232aab5fae5b1c76cb3c56ce8
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-08-07 12:12:40 +0200

    Refactor middleware

--- src/middleware.js
+++ src/middleware.js
@@ -1,7 +1,52 @@
-import { isRSAA, apiMiddleware } from 'redux-api-middleware'
+import {apiMiddleware, isRSAA} from 'redux-api-middleware'
 
-import { TOKEN_RECEIVED, refreshAccessToken } from './login/actions/auth'
-import { getRefreshToken, isAccessTokenExpired } from './login/reducers/auth'
+import {refreshAccessToken, TOKEN_RECEIVED} from './login/actions/auth'
+import {getRefreshToken, isAccessTokenExpired} from './login/reducers/auth'
+
+function checkNextAction (next, postponedRSAAs, rsaaMiddleware) {
+  return (nextAction) => {
+    // Run postponed actions after token refresh
+    if (nextAction.type === TOKEN_RECEIVED) {
+      console.debug('nextCheckPostponed')
+      console.debug(nextAction.type)
+      next(nextAction)
+      postponedRSAAs.forEach((postponed) => {
+        rsaaMiddleware(next)(postponed)
+      })
+    } else {
+      next(nextAction)
+    }
+  }
+}
+
+function processNextAction (postponedRSAAs, rsaaMiddleware) {
+  return (next) => (action) => {
+    const nextCheckPostponed = checkNextAction(next, postponedRSAAs, rsaaMiddleware)
+
+    if (isRSAA(action)) {
+      const refreshToken = getRefreshToken()
+      const isOauthEnabled = process.env.REACT_APP_OAUTH_ENABLED
+
+      if (isOauthEnabled === 'false') {
+        return rsaaMiddleware(next)(action)
+      }
+
+      if (refreshToken && isAccessTokenExpired()) {
+        console.debug('Access token is expired but we have refresh token')
+        postponedRSAAs.push(action)
+        console.debug('postponed RSAAs: ', postponedRSAAs)
+        if (postponedRSAAs.length > 0) {
+          return rsaaMiddleware(nextCheckPostponed)(refreshAccessToken())
+        } else {
+          return
+        }
+      }
+
+      return rsaaMiddleware(next)(action)
+    }
+    return next(action)
+  }
+}
 
 export function createApiMiddleware () {
   const postponedRSAAs = []
@@ -9,40 +54,7 @@ export function createApiMiddleware () {
   return ({ dispatch, getState }) => {
     const rsaaMiddleware = apiMiddleware({ dispatch, getState })
 
-    return (next) => (action) => {
-      const nextCheckPostponed = (nextAction) => {
-        // Run postponed actions after token refresh
-        if (nextAction.type === TOKEN_RECEIVED) {
-          console.debug('nextCheckPostponed')
-          console.debug(nextAction.type)
-          next(nextAction)
-          postponedRSAAs.forEach((postponed) => {
-            rsaaMiddleware(next)(postponed)
-          })
-        } else {
-          next(nextAction)
-        }
-      }
-
-      if (isRSAA(action)) {
-        console.debug('Is RSAA -> True')
-        const refreshToken = getRefreshToken()
-
-        if (refreshToken && isAccessTokenExpired()) {
-          console.debug('Access token is expired but we have refresh token')
-          postponedRSAAs.push(action)
-          console.debug('postponed RSAAs: ', postponedRSAAs)
-          if (postponedRSAAs.length > 0) {
-            return rsaaMiddleware(nextCheckPostponed)(refreshAccessToken())
-          } else {
-            return
-          }
-        }
-
-        return rsaaMiddleware(next)(action)
-      }
-      return next(action)
-    }
+    return processNextAction(postponedRSAAs, rsaaMiddleware)
   }
 }
 
