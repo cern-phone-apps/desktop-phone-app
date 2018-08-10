3d36ddd15006008db2502fd78bd5bea5f64af7b9
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-08-10 15:08:35 +0200

    Add tests for DisconnectModal and StatusSwitcher

--- src/__tests__/calls/components/StatusSwitcher/DisconnectModal.test.js
+++ src/__tests__/calls/components/StatusSwitcher/DisconnectModal.test.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import DisconnectModal from 'calls/components/StatusSwitcher/DisconnectModal'
 
-it('renders without crashing', () => {
-  const wrapper = shallow(<DisconnectModal disconnecting={true}/>)
-  // expect(wrapper.text()).toEqual('<Modal />')
-  // expect(wrapper.html()).toEqual('')
+describe('DisconnectModal component', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<DisconnectModal disconnecting={true}/>)
+    expect(wrapper.text()).toEqual('<Modal />')
+    expect(wrapper.debug()).toContain('Modal')
+    expect(wrapper.debug()).toContain('ModalContent')
+    expect(wrapper.debug()).toContain('Loader')
+  })
 })
