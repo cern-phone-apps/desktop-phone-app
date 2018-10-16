c9eb5991b15ba30364034d188de40646ac737377
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-10-16 11:29:00 +0200

    Update Caller and CallLoader

--- src/__tests__/calls/components/Caller/Caller.test.js
+++ src/__tests__/calls/components/Caller/Caller.test.js
@@ -7,19 +7,20 @@ describe('Caller Container', () => {
     const toggleDialpad = jest.fn()
     const makeCall = jest.fn()
     const wrapper = shallow(<Caller activeNumber={'12345'}
-      displayDialpad={false}
-      makeCall={makeCall}
-      searchValue={''}
-      t={key => key}
-      toggleDialpad={toggleDialpad}
-      updateSearchValue={updateSearch}
-      userSelected={false}/>)
+                                    displayDialpad={false}
+                                    makeCall={makeCall}
+                                    searchValue={''}
+                                    t={key => key}
+                                    toggleDialpad={toggleDialpad}
+                                    updateSearchValue={updateSearch}
+                                    userSelected={false}
+                                    onCall={false}
+                                    calling={false}/>)
 
-    expect(wrapper.text()).toEqual('header<Segment /><Segment /><Button />')
+    expect(wrapper.text()).toEqual('<Segment />')
     expect(wrapper.debug()).toContain('Segment')
-    expect(wrapper.debug()).toContain('Caller__ConnectedParagraph')
-    expect(wrapper.debug()).toContain('withRouter')
-    expect(wrapper.debug()).toContain('You are connected with number')
-    expect(wrapper.debug()).toContain('12345')
+    expect(wrapper.debug()).toContain('displayDialpadAction')
+    expect(wrapper.debug()).toContain('Connect')
+    expect(wrapper.debug()).toContain('UserSearch')
   })
 })
--- src/calls/components/Caller/Caller.js
+++ src/calls/components/Caller/Caller.js
@@ -1,20 +1,17 @@
 import React, {Component} from 'react'
-import {Button, Icon, Segment} from 'semantic-ui-react'
+import {Segment} from 'semantic-ui-react'
 import PropTypes from 'prop-types'
 import {translate} from 'react-i18next'
 
 import './Caller.css'
 import UserSearchContainer from 'calls/containers/components/UserSearch/UserSearchContainer'
-import DialpadContainer from 'calls/containers/components/Dialpad/DialpadContainer'
-import CalleeProfileContainer from 'calls/containers/components/CalleeProfile/CalleeProfileContainer'
-import ErrorMessageContainer from 'common/containers/components/ErrorMessage/ErrorMessageContainer'
 
 export class Caller extends Component {
   static propTypes = {
     userSelected: PropTypes.bool.isRequired,
+    onCall: PropTypes.bool.isRequired,
+    calling: PropTypes.bool.isRequired,
     t: PropTypes.func.isRequired,
-    updateSearchValue: PropTypes.func.isRequired,
-    searchValue: PropTypes.string.isRequired,
     activeNumber: PropTypes.string,
     makeCall: PropTypes.func.isRequired,
     toggleDialpad: PropTypes.func.isRequired,
@@ -25,27 +22,16 @@ export class Caller extends Component {
     this.props.toggleDialpad(!this.props.displayDialpad)
   }
 
-  render () {
-    const {t} = this.props
 
+  render () {
+
+    const segmentStyles = {
+      height: '100%'
+    }
     return (
-      <div className="call-inner-content">
-        <h2 className="ui center aligned header gray-text">{t('header')}</h2>
-        <Segment basic className="ui center aligned Caller__ConnectedParagraph">
-          <ErrorMessageContainer/>
-          <p>You are connected with number <strong>{this.props.activeNumber}</strong></p>
-        </Segment>
-        <Segment attached={!this.props.userSelected} className={'search-user'}>
-          <UserSearchContainer/>
-        </Segment>
-        {!this.props.userSelected &&
-        <Button attached='bottom' onClick={this.handleDialPadDisplayButton}>
-          <Icon name={'text telephone'}/> {t('dialpadText')}
-        </Button>
-        }
-        {this.props.displayDialpad && <DialpadContainer/>}
-        {this.props.userSelected && <CalleeProfileContainer/>}
-      </div>
+      <Segment basic style={segmentStyles}>
+        <UserSearchContainer displayDialpadAction={this.handleDialPadDisplayButton}/>
+      </Segment>
     )
   }
 }
