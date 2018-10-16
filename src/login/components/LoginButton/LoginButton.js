584e2e10361497f4e7baa9f705b28f2830a78a07
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-10-16 09:11:23 +0200

    Add piwik event to login

--- src/login/components/LoginButton/LoginButton.js
+++ src/login/components/LoginButton/LoginButton.js
@@ -3,6 +3,7 @@ import {Button} from 'semantic-ui-react'
 import PropTypes from 'prop-types'
 import buildUrl from 'build-url'
 import {translate} from 'react-i18next'
+import {logEvent} from 'common/utils'
 
 /**
  * The idea of this component is to redirect the user to the Oauth authorization URL of your provider.
@@ -41,6 +42,7 @@ export class LoginButton extends Component {
    * Redirects the user to the Oauth authorization URL
    */
   loginUser = () => {
+    logEvent('auth',`login`)
     const authorizeUrl = this.buildAuthorizeUrl()
     this.setState({
       redirected: true,
