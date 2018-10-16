439dac8cfbce3da7a82cfd3136a40ec5692191d0
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-10-16 09:05:06 +0200

    Add color property to LogoutButton

--- src/login/containers/components/LogoutButton/LogoutButtonContainer.js
+++ src/login/containers/components/LogoutButton/LogoutButtonContainer.js
@@ -3,22 +3,13 @@ import {withRouter} from 'react-router-dom'
 import {bindActionCreators} from 'redux'
 
 import * as authActionCreators from 'login/actions/auth'
-import {LogoutButton} from 'login/components/LogoutButton/LogoutButton'
+import LogoutButton from 'login/components/LogoutButton/LogoutButton'
 
 function mapDispatchToProps (dispatch) {
   return bindActionCreators({
     ...authActionCreators
   }, dispatch)
 }
-
-//
-// const mapStateToProps = (state) => ({
-//   lastRolledNumber: state.lastRolledNumber
-// });
-
-// const mapDispatchToProps = (dispatch) => ({
-//   onRollDice: () => dispatch({ type: 'ROLL_DICE' })
-// });
 
 export const LogoutButtonContainer = connect(
   null,
