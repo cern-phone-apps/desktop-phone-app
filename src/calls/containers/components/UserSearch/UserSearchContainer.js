5c430d3e5b225372ee622286b8ebb236e8c6326b
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-10-16 11:34:43 +0200

    Update StatusSwitcher

--- src/calls/containers/components/UserSearch/UserSearchContainer.js
+++ src/calls/containers/components/UserSearch/UserSearchContainer.js
@@ -2,21 +2,25 @@ import { bindActionCreators } from 'redux'
 
 import { connect } from 'react-redux'
 import * as searchActionCreators from 'calls/actions/search'
+import * as dialpadActionCreators from 'calls/actions/dialpad'
 import { withRouter } from 'react-router-dom'
 import UserSearch from 'calls/components/UserSearch/UserSearch'
 
 function mapStateToProps ({calls}) {
   return {
     userSelected: calls.search.userSelected,
-    value: calls.search.value,
+    dialpadValue: calls.dialpad.value,
     results: calls.search.searchResults,
-    displayDialpad: calls.dialpad.display
+    displayDialpad: calls.dialpad.display,
+    onCall: calls.call.onCall,
+    calling: calls.call.calling
   }
 }
 
 function mapDispatchToProps (dispatch) {
   return bindActionCreators({
-    ...searchActionCreators
+    ...searchActionCreators,
+    ...dialpadActionCreators
   },
   dispatch)
 }
@@ -26,4 +30,4 @@ export const UserSearchContainer = connect(
   mapDispatchToProps
 )(UserSearch)
 
-export default withRouter(UserSearchContainer)
+export default UserSearchContainer
