dd5c4bc8974d6bcb45dd0b3ea27bdfdaad8cc3b1
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-10-16 11:41:21 +0200

    Update call and Dialpad reducers

--- src/calls/reducers/dialpad.js
+++ src/calls/reducers/dialpad.js
@@ -1,11 +1,17 @@
 import * as dialpadActions from 'calls/actions/dialpad'
 
 const initialState = {
-  display: false
+  display: false,
+  value: ''
 }
 
 export default (state = initialState, action) => {
   switch (action.type) {
+    case dialpadActions.DIALPAD_NUMBER_UPDATED:
+      return {
+        ...state,
+        value: action.newValue
+      }
     case dialpadActions.TOGGLE_DIALPAD:
       return {
         ...state,
