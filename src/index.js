aa416a867a1e2be9ebdd068345544800d03d789a
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-10-26 16:59:12 +0200

    Update Calls Screen and Main Page

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
-import {ConnectedRouter} from 'react-router-redux'
+import { Provider } from 'react-redux'
+import { ConnectedRouter } from 'react-router-redux'
 import createHistory from 'history/createBrowserHistory'
-import {PersistGate} from 'redux-persist/es/integration/react'
-import {I18nextProvider} from 'react-i18next'
+import { PersistGate } from 'redux-persist/es/integration/react'
+import { I18nextProvider } from 'react-i18next'
 import ReactPiwik from 'react-piwik'
 
-
-
-/**
+/** s
  * Local imports
  */
 import 'semantic-ui-css/semantic.min.css'
@@ -29,26 +27,28 @@ import PhoneProvider from 'calls/providers/PhoneProvider/PhoneProvider'
 const piwik = new ReactPiwik({
   url: process.env.REACT_APP_PIWIK_URL,
   siteId: process.env.REACT_APP_PIWIK_SITE_ID,
-  trackErrors: true,
-});
+  trackErrors: true
+})
 
 /**
  * Set up the store and the history
  */
 const history = createHistory()
-const {store, persistor} = configureStore(history)
+const { store, persistor } = configureStore(history)
 
 ReactDOM.render(
   <Provider store={store}>
     <I18nextProvider i18n={i18n}>
-      <PersistGate loading={<div/>} persistor={persistor}>
+      <PersistGate loading={<div />} persistor={persistor}>
         {/* Need to be here, before ConnectedRouter */}
         <PhoneProvider>
           <ConnectedRouter history={piwik.connectToHistory(history)}>
-            <App/>
+            <App />
           </ConnectedRouter>
         </PhoneProvider>
       </PersistGate>
     </I18nextProvider>
-  </Provider>, document.getElementById('root'))
+  </Provider>,
+  document.getElementById('root')
+)
 registerServiceWorker()
