0e50aafb9d6f2dcfebbf35a62da0cd6cc956f2b3
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-10-26 15:57:55 +0200

    Update eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,49 +1,23 @@
 module.exports = {
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    "es6": true,
-    "jest": true
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    jest: true
   },
-  "settings": {
-    "import/resolver": {
-      "node": {
-        "paths": ["./src"]
+  settings: {
+    'import/resolver': {
+      node: {
+        paths: ['./src']
       }
     }
   },
-  "overrides": [
-    {
-      "files": [ "bin/*.js", "lib/*.js" ],
-      "excludedFiles": ["*.test.js"],
-      "rules": {
-        "quotes": [ 2, "single" ]
-      }
-    }
-  ],
-  "extends": [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "standard"
-  ],
-  "parser": "babel-eslint",
-  "parserOptions": {
-    "ecmaVersion": 6,
-    "ecmaFeatures": {
-      "jsx": true
-    },
-    "sourceType": "module"
-  },
-  "plugins": [
-    "react",
-    "babel"
-  ],
-  "rules": {
-    "linebreak-style": [
-      "error",
-      "unix"
-    ],
-    "no-console": 0,
-    "babel/no-invalid-this": 1
+  extends: ['eslint:recommended', 'plugin:react/recommended'],
+  parser: 'babel-eslint',
+  plugins: ['react', 'babel'],
+  rules: {
+    'linebreak-style': ['error', 'unix'],
+    'no-console': 0,
+    'babel/no-invalid-this': 1
   }
 };
