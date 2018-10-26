8267a4455d8c95b41b16db38f2f6df31f4ba5843
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-10-26 16:57:07 +0200

    Update call loader

--- src/__tests__/calls/components/CallLoader/CallLoader.test.js
+++ src/__tests__/calls/components/CallLoader/CallLoader.test.js
@@ -4,13 +4,13 @@ import {CallLoader} from 'calls/components/CallLoader/CallLoader'
 describe('CalleeProfile Component tests', () => {
   it('renders CalleeProfile without crashing', () => {
     const acceptOutgoingCall = jest.fn()
-    const hangUpCall = jest.fn()
+    const hangUpCurrentCall = jest.fn()
     const wrapper = shallow(<CallLoader
       t={key => key}
       recipientName={'example'}
       acceptOutgoingCall={acceptOutgoingCall}
       calling
-      hangupCall={hangUpCall}
+      hangupCall={hangUpCurrentCall}
       phoneNumber={'12345'}
      phoneService={{}}/>)
     expect(wrapper.text()).toEqual('<Segment />')
