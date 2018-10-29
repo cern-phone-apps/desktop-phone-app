3a4eeb86cc987b5ed0de834752a44e5711da51e8
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-10-29 12:17:11 +0100

    Refactor OnCallMessage

--- src/calls/components/OnCallMessage/OnCallMessageContainer.js
+++ src/calls/components/OnCallMessage/OnCallMessageContainer.js
@@ -1,7 +1,4 @@
-import { bindActionCreators } from 'redux'
-
 import { connect } from 'react-redux'
-import * as callActionCreators from 'calls/actions/call'
 import { withRouter } from 'react-router-dom'
 import OnCallMessage from 'calls/components/OnCallMessage/OnCallMessage'
 
@@ -11,11 +8,7 @@ function mapStateToProps ({calls}) {
   }
 }
 
-function mapDispatchToProps (dispatch) {
-  return bindActionCreators(callActionCreators, dispatch)
-}
-
 export default withRouter(connect(
   mapStateToProps,
-  mapDispatchToProps
+  null
 )(OnCallMessage))
