4446908867ecca05af903cefc586fc83b5aa6605
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-10-29 11:49:43 +0100

    Remove unused component

--- src/__tests__/calls/components/NotConnectedScreen/NotConnectedScreen.test.js
+++ src/__tests__/calls/components/NotConnectedScreen/NotConnectedScreen.test.js
@@ -1,13 +1,12 @@
-import React from 'react'
-import {NotConnectedScreen} from 'calls/components/NotConnectedScreen/NotConnectedScreen'
+import React from "react";
+import { NotConnectedScreen } from "calls/components/NotConnectedScreen/NotConnectedScreen";
 
-describe('NotConnectedScreen component', () => {
-  it('renders without crashing', () => {
-    const wrapper = shallow(<NotConnectedScreen
-      t={key=>key}/>)
+describe("NotConnectedScreen component", () => {
+  it("renders without crashing", () => {
+    const wrapper = shallow(<NotConnectedScreen t={key => key} />);
 
-    expect(wrapper.text()).toEqual('<Segment />')
-    expect(wrapper.debug()).toContain('basic')
-    expect(wrapper.debug()).toContain('center')
-  })
-})
+    expect(wrapper.text()).toEqual("<Segment />");
+    expect(wrapper.debug()).toContain("basic");
+    expect(wrapper.debug()).toContain("center");
+  });
+});
