47d01df36a1dbf4a862dc5729bce7bfba0f0f173
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-10-30 10:44:34 +0100

    Prettify root js files

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { Provider } from 'react-redux'
-import { ConnectedRouter } from 'react-router-redux'
-import createHistory from 'history/createBrowserHistory'
-import { PersistGate } from 'redux-persist/es/integration/react'
-import { I18nextProvider } from 'react-i18next'
-import ReactPiwik from 'react-piwik'
-
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { ConnectedRouter } from "react-router-redux";
+import createHistory from "history/createBrowserHistory";
+import { PersistGate } from "redux-persist/es/integration/react";
+import { I18nextProvider } from "react-i18next";
+import ReactPiwik from "react-piwik";
 /** s
  * Local imports
  */
-import 'semantic-ui-css/semantic.min.css'
-import './index.css'
-import registerServiceWorker from './registerServiceWorker'
-import configureStore from 'store'
-import i18n from 'i18n'
-import App from 'App'
+import "semantic-ui-css/semantic.min.css";
+import "./index.css";
+import registerServiceWorker from "./registerServiceWorker";
+import configureStore from "store";
+import i18n from "i18n";
+import App from "App";
+import PhoneProvider from "calls/providers/PhoneProvider/PhoneProvider";
 // We need the PhoneProvider to be available in the whole app in order to make and receive calls
-
-import PhoneProvider from 'calls/providers/PhoneProvider/PhoneProvider'
 
 /**
  * Set up Sentry and Piwik analytics
@@ -28,13 +26,13 @@ const piwik = new ReactPiwik({
   url: process.env.REACT_APP_PIWIK_URL,
   siteId: process.env.REACT_APP_PIWIK_SITE_ID,
   trackErrors: true
-})
+});
 
 /**
  * Set up the store and the history
  */
-const history = createHistory()
-const { store, persistor } = configureStore(history)
+const history = createHistory();
+const { store, persistor } = configureStore(history);
 
 ReactDOM.render(
   <Provider store={store}>
@@ -49,6 +47,6 @@ ReactDOM.render(
       </PersistGate>
     </I18nextProvider>
   </Provider>,
-  document.getElementById('root')
-)
-registerServiceWorker()
+  document.getElementById("root")
+);
+registerServiceWorker();
--- src/middleware.js
+++ src/middleware.js
@@ -1,66 +1,73 @@
-import {apiMiddleware, isRSAA, RSAA} from 'redux-api-middleware'
+import { apiMiddleware, isRSAA, RSAA } from "redux-api-middleware";
 
+import {
+  LOGOUT_REQUEST,
+  refreshAccessToken,
+  TOKEN_RECEIVED
+} from "./login/actions/auth";
+import { getRefreshToken, isAccessTokenExpired } from "./login/reducers/auth";
 
-import {LOGOUT_REQUEST, refreshAccessToken, TOKEN_RECEIVED} from './login/actions/auth'
-import {getRefreshToken, isAccessTokenExpired} from './login/reducers/auth'
-
-function checkNextAction (next, postponedRSAAs, rsaaMiddleware) {
-  return (nextAction) => {
+function checkNextAction(next, postponedRSAAs, rsaaMiddleware) {
+  return nextAction => {
     // Run postponed actions after token refresh
     if (nextAction.type === TOKEN_RECEIVED) {
-      console.debug('nextCheckPostponed')
-      console.debug(nextAction.type)
-      next(nextAction)
-      postponedRSAAs.forEach((postponed) => {
-        rsaaMiddleware(next)(postponed)
-      })
+      console.debug("nextCheckPostponed");
+      console.debug(nextAction.type);
+      next(nextAction);
+      postponedRSAAs.forEach(postponed => {
+        rsaaMiddleware(next)(postponed);
+      });
     } else {
-      next(nextAction)
+      next(nextAction);
     }
-  }
+  };
 }
 
-function processNextAction (postponedRSAAs, rsaaMiddleware) {
-  return (next) => (action) => {
-    const nextCheckPostponed = checkNextAction(next, postponedRSAAs, rsaaMiddleware)
+function processNextAction(postponedRSAAs, rsaaMiddleware) {
+  return next => action => {
+    const nextCheckPostponed = checkNextAction(
+      next,
+      postponedRSAAs,
+      rsaaMiddleware
+    );
 
     if (isRSAA(action)) {
-      const refreshToken = getRefreshToken()
-      const isOauthEnabled = process.env.REACT_APP_OAUTH_ENABLED
+      const refreshToken = getRefreshToken();
+      const isOauthEnabled = process.env.REACT_APP_OAUTH_ENABLED;
 
-      if (isOauthEnabled === 'false') {
-        return rsaaMiddleware(next)(action)
+      if (isOauthEnabled === "false") {
+        return rsaaMiddleware(next)(action);
       }
       // If it is a LOGOUT_REQUEST we don't try to refresh the token
-      if(action[RSAA].types.indexOf(LOGOUT_REQUEST) > -1){
-        return rsaaMiddleware(next)(action)
+      if (action[RSAA].types.indexOf(LOGOUT_REQUEST) > -1) {
+        return rsaaMiddleware(next)(action);
       }
 
       if (refreshToken && isAccessTokenExpired()) {
-        console.debug('Access token is expired but we have refresh token')
-        postponedRSAAs.push(action)
-        console.debug('postponed RSAAs: ', postponedRSAAs)
+        console.debug("Access token is expired but we have refresh token");
+        postponedRSAAs.push(action);
+        console.debug("postponed RSAAs: ", postponedRSAAs);
         if (postponedRSAAs.length > 0) {
-          return rsaaMiddleware(nextCheckPostponed)(refreshAccessToken())
+          return rsaaMiddleware(nextCheckPostponed)(refreshAccessToken());
         } else {
-          return
+          return;
         }
       }
 
-      return rsaaMiddleware(next)(action)
+      return rsaaMiddleware(next)(action);
     }
-    return next(action)
-  }
+    return next(action);
+  };
 }
 
-export function createApiMiddleware () {
-  const postponedRSAAs = []
+export function createApiMiddleware() {
+  const postponedRSAAs = [];
 
   return ({ dispatch, getState }) => {
-    const rsaaMiddleware = apiMiddleware({ dispatch, getState })
+    const rsaaMiddleware = apiMiddleware({ dispatch, getState });
 
-    return processNextAction(postponedRSAAs, rsaaMiddleware)
-  }
+    return processNextAction(postponedRSAAs, rsaaMiddleware);
+  };
 }
 
-export default createApiMiddleware()
+export default createApiMiddleware();
