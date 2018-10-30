cde5f6e4745f83905a5840ff7a1196e2ddd561bb
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-10-30 10:50:51 +0100

    Remove unused imports

--- src/calls/components/Caller/CallerContainer.js
+++ src/calls/components/Caller/CallerContainer.js
@@ -1,7 +1,4 @@
-import { bindActionCreators } from 'redux'
-
 import { connect } from 'react-redux'
-import {toggleDialpad} from "calls/actions/dialpad";
 import Caller from 'calls/components/Caller/Caller'
 
 function mapStateToProps ({calls}) {
@@ -10,11 +7,6 @@ function mapStateToProps ({calls}) {
   }
 }
 
-// function mapDispatchToProps (dispatch) {
-//   return bindActionCreators({
-//     toggleDialpad
-//   }, dispatch)
-// }
 
 export default connect(
   mapStateToProps,
--- src/calls/components/OnCallDetails/OnCallDetailsContainer.js
+++ src/calls/components/OnCallDetails/OnCallDetailsContainer.js
@@ -1,5 +1,4 @@
 import { connect } from "react-redux";
-import { withRouter } from "react-router-dom";
 import OnCallDetails from "calls/components/OnCallDetails/OnCallDetails";
 import { phoneService } from "calls/providers/PhoneProvider/PhoneProvider";
 
