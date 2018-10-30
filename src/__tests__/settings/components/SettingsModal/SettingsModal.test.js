67cac4e135c8218e7ccc8ff74c0eb135fbdcc864
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-10-30 10:27:36 +0100

    Rename SettingsModal

---
+++ src/__tests__/settings/components/SettingsModal/SettingsModal.test.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { SettingsModal } from "settings/components/SettingsModal/SettingsModal";
+
+it("renders without crashing", () => {
+  const hideSidebarIfVisible = jest.fn();
+  const logout = jest.fn();
+  const wrapper = shallow(
+    <SettingsModal
+      t={key => key}
+      hideSidebarIfVisible={hideSidebarIfVisible}
+      logout={logout}
+    />
+  );
+
+  expect(wrapper.text()).toContain("<Modal />");
+  expect(wrapper.html()).toContain('<a class="item">');
+  expect(wrapper.html()).toContain("Settings");
+});
