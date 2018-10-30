5c45fc97ff2ea4cbb62292feb03a14ddb87adbdd
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-10-30 10:19:53 +0100

    Update ErrorMessage

--- src/common/components/ErrorMessage/ErrorMessage.js
+++ src/common/components/ErrorMessage/ErrorMessage.js
@@ -1,42 +1,52 @@
-import React, {Component} from 'react'
-import PropTypes from 'prop-types'
-import {translate} from 'react-i18next'
-import {Button, Icon, Message, Modal, Segment} from 'semantic-ui-react'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { translate } from "react-i18next";
+import { Button, Icon, Message, Modal, Segment } from "semantic-ui-react";
 
 export class ErrorMessage extends Component {
   static propTypes = {
     errors: PropTypes.array,
     t: PropTypes.func.isRequired
-  }
+  };
 
   styles = {
     margin: 0,
-    padding: '0 0 1em 0'
-  }
+    padding: "0 0 1em 0"
+  };
 
-  render () {
-    const {errors} = this.props
+  render() {
+    const { errors } = this.props;
 
-    let results = errors.filter((error) => (error && error.statusCode))
+    let results = errors.filter(error => error && error.statusCode);
 
     if (results.length < 1) {
-      return ''
+      return "";
     }
     return (
-      <Modal trigger={<Button as={'a'} className={'flat'}><Icon color={'red'} name={'warning'} /></Button> }>
+      <Modal
+        trigger={
+          <Button as={"a"} className={"flat"}>
+            <Icon color={"red"} name={"warning"} />
+          </Button>
+        }
+      >
         <Modal.Header>Errors</Modal.Header>
         <Modal.Content>
           <Modal.Description>
-            <Segment textAlign={'center'} basic style={this.styles}>
+            <Segment textAlign={"center"} basic style={this.styles}>
               {results.map((element, index) => {
-                return <Message color='red' key={`message${index}`}>{element.message} ({element.statusCode})</Message>
+                return (
+                  <Message color="red" key={`message${index}`}>
+                    {element.message} ({element.statusCode})
+                  </Message>
+                );
               })}
             </Segment>
           </Modal.Description>
         </Modal.Content>
       </Modal>
-    )
+    );
   }
 }
 
-export default translate('calls')(ErrorMessage)
+export default translate("calls")(ErrorMessage);
