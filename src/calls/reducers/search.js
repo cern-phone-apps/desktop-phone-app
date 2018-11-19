5b7c033986c8c129e98bb37ca1f6c7fa95ac59ee
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-11-19 14:06:29 +0100

    Add cypress and travis configuration
    Update travis ci
    
    Add cypress npm commands
    
    Dont use print-evn
    
    Add cypress project id
    
    Update travis
    
    Change name of cypress commands
    
    Add record flag to cypress:run
    
    Change command on travis
    
    Add cypress:verify to install script
    
    Add parallel run on travis
    
    Add 2 parallel jobs to travis
    
    Fix travis
    
    Fix travis command
    
    Remove parallel run on travis
    
    Fix identation on travis
    
    Run all tests on one stage on travis
    
    Add build to install stage
    
    Add again 2 test stages
    
    Remove names to stages
    
    Move install to stages
    
    Rename jobs
    
    Fix code styles

--- src/calls/reducers/search.js
+++ src/calls/reducers/search.js
@@ -1,11 +1,12 @@
-import * as searchActions from 'calls/actions/search'
+import * as searchActions from "calls/actions/search";
+import { logMessage } from "common/utils";
 
 const initialState = {
   userSelected: false,
   searchResults: [],
   searching: false,
   searchEnable: false
-}
+};
 
 /**
  * Formats an array of users to a format for the search results field.
@@ -14,23 +15,23 @@ const initialState = {
  * @param usersArray Array of dict
  * @returns {*} Array of dict with the users formatted.
  */
-export function getUsersFormattedForSearch (usersArray) {
+export function getUsersFormattedForSearch(usersArray) {
   if (usersArray === undefined) {
-    return []
+    return [];
   }
 
-  return usersArray.slice(0, 9).map(function (user, index) {
-    const division = user.division === '[]' ? '' : user.division
-    const group = user.cernGroup === '[]' ? '' : `-${user.cernGroup}`
-    const section = user.cernSection === '[]' ? '' : `-${user.cernSection}`
-    const displayName = user.displayName
+  return usersArray.slice(0, 9).map(function(user, index) {
+    const division = user.division === "[]" ? "" : user.division;
+    const group = user.cernGroup === "[]" ? "" : `-${user.cernGroup}`;
+    const section = user.cernSection === "[]" ? "" : `-${user.cernSection}`;
+    const displayName = user.displayName;
     return {
       index: index,
       title: displayName,
       description: `${division}${group}${section}`,
       username: user.username
-    }
-  })
+    };
+  });
 }
 
 /**
@@ -41,53 +42,53 @@ export function getUsersFormattedForSearch (usersArray) {
  * @returns {{userSelected: boolean, value: string, searchResults: Array}}
  */
 const search = (state = initialState, action) => {
-  console.debug(`Calling call reducer: ${action.type}`)
+  logMessage(`Calling call reducer: ${action.type}`);
   switch (action.type) {
     case searchActions.USER_SELECTED:
       return {
         ...state,
         user: action.user,
         userSelected: true
-      }
+      };
     case searchActions.USER_NOT_SELECTED:
       return {
         ...state,
         userSelected: false
-      }
+      };
     case searchActions.SEARCH_REQUEST:
       return {
         ...state,
         searching: true,
         searchEnable: true,
         searchResults: []
-      }
+      };
     case searchActions.SEARCH_END:
       return {
         ...state,
         searching: false,
         searchResults: [],
         searchEnable: false
-      }
+      };
     case searchActions.SEARCH_CLEAR:
       return {
         ...state,
         searchResults: []
-      }
+      };
     case searchActions.SEARCH_SUCCESS:
       return {
         ...state,
         searchResults: getUsersFormattedForSearch(action.payload.result),
         searching: false
-      }
+      };
     case searchActions.SEARCH_FAILURE:
       return {
         ...state,
         searching: false
-      }
+      };
 
     default:
-      return state
+      return state;
   }
-}
+};
 
-export default search
+export default search;
