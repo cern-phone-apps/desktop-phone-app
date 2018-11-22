3a08a8e87b5b17801fc5483ebb3a520adee85d8a
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-11-22 16:59:57 +0100

    Display error message on connect number button

--- src/__tests__/calls/components/ConnectNumberButton/ConnectNumberButton.test.js
+++ src/__tests__/calls/components/ConnectNumberButton/ConnectNumberButton.test.js
@@ -1,18 +1,21 @@
-import React from 'react'
-import ConnectNumberButton from 'calls/components/ConnectNumberButton/ConnectNumberButton'
+import React from "react";
+import ConnectNumberButton from "calls/components/ConnectNumberButton/ConnectNumberButton";
 
-describe('ConnectNumberButton Container', () => {
-  it('renders without crashing', () => {
-    const getUserPhoneNumbers = jest.fn()
-    const setActiveNumber = jest.fn()
-    const wrapper = shallow(<ConnectNumberButton
-      connecting={false}
-      getUserPhoneNumbers={getUserPhoneNumbers}
-      numbers={['1', '2', '3']}
-      setActiveNumber={setActiveNumber} phoneNumber={'12345'} phoneService={{}}/>)
+describe("ConnectNumberButton Container", () => {
+  it("renders without crashing", () => {
+    const getUserPhoneNumbers = jest.fn();
+    const setActiveNumber = jest.fn();
+    const wrapper = shallow(
+      <ConnectNumberButton
+        connecting={false}
+        getUserPhoneNumbers={getUserPhoneNumbers}
+        setActiveNumber={setActiveNumber}
+        phoneService={{}}
+       />
+    );
 
-    expect(wrapper.text()).toEqual('<ButtonNumbersList />')
-    expect(wrapper.html()).toContain('button')
-    expect(wrapper.html()).toContain('plug')
-  })
-})
+    expect(wrapper.text()).toEqual("<Segment />");
+    expect(wrapper.html()).toContain("segment");
+    expect(wrapper.html()).toContain("Loading phone numbers...");
+  });
+});
