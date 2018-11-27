697ff142823b28cd21d99c857251a2a8eab75dd0
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-11-27 14:34:57 +0100

    Clear cookies and local storage before each test run

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -18,3 +18,9 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+
+beforeEach(function() {
+  cy.log("Before each...");
+  cy.clearCookies();
+  cy.clearLocalStorage();
+});
