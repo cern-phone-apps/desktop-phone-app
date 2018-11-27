7299f12ccc95ff91fc3ddd1b5158803b16eb3594
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-11-27 14:34:39 +0100

    Update mock_server.js

--- cypress/utils/mock_server.js
+++ cypress/utils/mock_server.js
@@ -16,8 +16,10 @@ const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.use(morgan("dev"));
 app.use(cookieParser());
+app.disable('etag');
 
 app.use(function(req, res, next) {
+  res.type("application/json");
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header(
     "Access-Control-Allow-Headers",
@@ -47,40 +49,33 @@ app.get("/OAuth/Authorize", urlencodedParser, (req, res, next) => {
   // }
 });
 
-app.post(
-  "/auth/v1/login/",
-  (req, res, next) => {
-    console.log("Setting up cookie");
-    res.cookie("csrf_access_token", "abcd1234", { httpOnly: false });
-    res.cookie("csrf_refresh_token", "abcd1234", { httpOnly: false });
-    next();
-  },
-  (req, res, next) => {
-    res.json({
-      login: true
-    });
-    console.log("Cookies: ", req.cookies);
-    res.send();
-  }
-);
+app.post("/auth/v1/login/", (req, res, next) => {
+  // console.log("Cookies: ", req.cookies);
+  res.clearCookie("csrf_access_token");
+  res.clearCookie("csrf_refresh_token");
+  // console.log("LOOK HERE");
+  // console.log(res);
+  res.cookie("csrf_access_token", "abcd1234", { httpOnly: false });
+  res.cookie("csrf_refresh_token", "abcd1234", { httpOnly: false });
+  res.json({
+    login: true
+  });
+  // console.log("RES 2");
+  // console.log(res);
+  res.send();
+});
 
-app.delete(
-  "/auth/v1/logout/",
-  (req, res, next) => {
-    console.log("Setting up cookie");
-    res.cookie("csrf_access_token", null, { httpOnly: false });
-    res.cookie("csrf_refresh_token", null, { httpOnly: false });
-    next();
-  },
-  (req, res, next) => {
-    res.json({
+app.delete("/auth/v1/logout/", (req, res, next) => {
+  console.log("Cookies: ", req.cookies);
+  // cookies.set('csrf_access_token', 'abcd1234');
+  res.clearCookie("csrf_access_token");
+  res.clearCookie("csrf_refresh_token");
+  res
+    .json({
       logout: true
-    });
-    console.log("Cookies: ", req.cookies);
-    // cookies.set('csrf_access_token', 'abcd1234');
-    res.send();
-  }
-);
+    })
+    .send();
+});
 
 app.get("/api/v1/users/me/", (req, res, next) => {
   res.json({
@@ -91,7 +86,7 @@ app.get("/api/v1/users/me/", (req, res, next) => {
     email: "john.doe@cern.ch",
     username: "johndoe55"
   });
-
+  console.log(req.cookies);
   res.send();
 });
 
@@ -208,24 +203,24 @@ app.get("/api/v1/users/search/", (req, res, next) => {
   res.send();
 });
 
-
 app.get("/api/v1/users/", (req, res, next) => {
   res.json({
-    "result":
-      {
-        "displayName": "Test 4",
-        "phones": [{
-          "number": "88004",
-          "phoneType": "phone"
-        }],
-        "mail": "test4@cern.ch",
-        "cernSection": "TEST_SEC",
-        "cernGroup": "TEST_GROUP",
-        "division": "TEST_DIVISION",
-        "physicalDeliveryOfficeName": "No office",
-        "username": "test80004",
-        "personId": "test80004"
-      }
+    result: {
+      displayName: "Test 4",
+      phones: [
+        {
+          number: "88004",
+          phoneType: "phone"
+        }
+      ],
+      mail: "test4@cern.ch",
+      cernSection: "TEST_SEC",
+      cernGroup: "TEST_GROUP",
+      division: "TEST_DIVISION",
+      physicalDeliveryOfficeName: "No office",
+      username: "test80004",
+      personId: "test80004"
+    }
   });
 
   res.send();
