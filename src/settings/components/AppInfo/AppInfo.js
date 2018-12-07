20bf299228320e5520842531213804d7d6da9fbb
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-12-07 10:38:36 +0100

    Update app version

--- src/settings/components/AppInfo/AppInfo.js
+++ src/settings/components/AppInfo/AppInfo.js
@@ -1,26 +1,25 @@
-import React from 'react'
-import {Header} from 'semantic-ui-react'
-import {translate} from 'react-i18next'
-import ErrorBoundary from 'common/components/ErrorBoundary/ErrorBoundary'
+import React from "react";
+import { Header } from "semantic-ui-react";
+import { translate } from "react-i18next";
+import ErrorBoundary from "common/components/ErrorBoundary/ErrorBoundary";
 
+const APP_VERSION = "v0.1.2";
 
-const APP_VERSION = 'v0.1.0'
-
-export const AppInfo = ({t}) => {
+export const AppInfo = ({ t }) => {
   return (
     <div>
       <ErrorBoundary>
-        <Header as={'h4'}>{t('appInfo.header')}</Header>
+        <Header as={"h4"}>{t("appInfo.header")}</Header>
         <ul>
           <li>
-            <strong>{t('appInfo.version')}</strong> {APP_VERSION}
+            <strong>{t("appInfo.version")}</strong> {APP_VERSION}
           </li>
         </ul>
       </ErrorBoundary>
     </div>
-  )
-}
+  );
+};
 
-AppInfo.propTypes = {}
+AppInfo.propTypes = {};
 
-export default translate('settings')(AppInfo)
+export default translate("settings")(AppInfo);
