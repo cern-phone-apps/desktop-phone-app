638acc3bddfd0d6721e970166b1428d4be91d39b
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-12-11 11:46:40 +0100

    Remove unused variable

--- src/settings/components/SettingsModal/SettingsModal.js
+++ src/settings/components/SettingsModal/SettingsModal.js
@@ -5,7 +5,6 @@ import { Icon, Modal, Menu, Header } from "semantic-ui-react";
 
 import DeviceSettings from "settings/components/DeviceSettings/DeviceSettings";
 import PersonalInfoContainer from "settings/components/PersonalInfo/PersonalInfoContainer";
-import LanguageSettings from "settings/components/LanguageSettings/LanguageSettings";
 import AppInfo from "settings/components/AppInfo/AppInfo";
 import NotificationsSettings from "settings/components/NotificationsSettings/NotificationsSettings";
 import LogoutButtonContainer from "login/components/LogoutButton/LogoutButtonContainer";
