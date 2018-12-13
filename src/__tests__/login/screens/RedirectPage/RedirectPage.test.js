aeb33f76ff2deaa3cb34cb09fd9e84545e905fde
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-12-13 10:24:10 +0100

    Add codeclimate yaml

--- src/__tests__/login/screens/RedirectPage/RedirectPage.test.js
+++ src/__tests__/login/screens/RedirectPage/RedirectPage.test.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import RedirectPage from 'login/screens/RedirectPage/RedirectPage'
+import React from "react";
+import RedirectPage from "login/screens/RedirectPage/RedirectPage";
 
-it('renders without crashing', () => {
-  const login = async (code) => {
-    return {loggedIn: true}
-  }
-  const getMe = jest.fn()
-  const wrapper = shallow(<RedirectPage
-    login={login}
-    isAuthenticated={false}
-    urlQuery={''}
-    getMe={getMe}
-    loginInProgress={false}
-  />)
+it("renders without crashing", () => {
+  const login = async code => {
+    return { loggedIn: true };
+  };
+  const getMe = jest.fn();
+  const wrapper = shallow(
+    <RedirectPage
+      login={login}
+      isAuthenticated={false}
+      urlQuery={""}
+      getMe={getMe}
+      loginInProgress={false}
+    />
+  );
 
-  expect(wrapper.text()).toEqual('<Redirect />')
-})
+  expect(wrapper.text()).toEqual("<Redirect />");
+});
