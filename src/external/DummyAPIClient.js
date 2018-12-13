0e20b148462feabd534222b790260c2323abbe39
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-12-13 15:23:01 +0100

    Add tests to DummyAPIClient

--- src/external/DummyAPIClient.js
+++ src/external/DummyAPIClient.js
@@ -32,6 +32,7 @@ export class Dial {
 
   sendEvent(event) {
     this.dialNotifier.emit("ToneEvent", event);
+    return event;
   }
 
   getNotifier() {
@@ -40,35 +41,43 @@ export class Dial {
 
   authenticate(user, token) {
     if (user && token) {
+      const event = Dial.buildEvent("registered", {});
+
       setTimeout(() => {
-        const event = Dial.buildEvent("registered", {});
         this.sendEvent(event);
       }, 300);
+      return event;
     } else throw Error("Cannot authenticate. Token or User not set.");
+
   }
 
   call(callee) {
     if (!this.media) {
       throw Error("Cannot launch call. Media element not set.");
     }
+    const event = Dial.buildEvent("accepted", {});
+
     setTimeout(() => {
-      const event = Dial.buildEvent("accepted", {});
       this.sendEvent(event);
     }, 300);
+    return event;
   }
 
   hangUp() {
     const event = Dial.buildEvent("terminated", {});
     this.sendEvent(event);
+    return event;
   }
 
   answer() {
     const event = Dial.buildEvent("Invite accepted", {});
     this.sendEvent(event);
+    return event;
   }
 
   sendDTMF(tone) {
     infoMessage(`Sending DTMF message: ${tone}`);
+    return tone;
   }
 
   stopAgent = () => {
@@ -78,5 +87,6 @@ export class Dial {
       const event = Dial.buildEvent("unregistered", {}, cause, response);
       this.sendEvent(event);
     }, 300);
+    return true;
   };
 }
