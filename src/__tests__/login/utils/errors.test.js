4f8fc3b39f41732573c88b04154a6264b1bd70d1
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2018-12-13 09:37:05 +0100

    Refactor and test common/utils/errors

--- src/__tests__/login/utils/errors.test.js
+++ src/__tests__/login/utils/errors.test.js
@@ -1,14 +1,132 @@
 import React from "react";
-import Cookies from "js-cookie";
-
 import {
-  getAccessToken,
-} from "login/utils/tokens";
+  buildApiErrorFromAction,
+  buildError, errors,
+  handleErrorResponse,
+  handleErrorWithLogin,
+  handleErrorWithToken
+} from "login/utils/errors";
 
-const cookiesGet = Cookies.get;
+it("generates a default error", () => {
+  const error = buildError();
+  expect(error).toEqual({ message: "Unknown error", statusCode: 999 });
+});
 
-it("is able to retrieve access token undefined", () => {
-  Cookies.get = cookiesGet;
-  const token = getAccessToken();
-  expect(token).toBe(undefined);
+it("generates a custom error", () => {
+  const error = buildError("Error message", 1234);
+  expect(error).toEqual({ message: "Error message", statusCode: 1234 });
+});
+
+it("generates error from action", () => {
+  const action = {
+    payload: {
+      message: "Hello Error",
+      status: 12345
+    }
+  };
+
+  const error = buildApiErrorFromAction(action);
+  expect(error).toEqual({ message: "Hello Error", statusCode: 12345 });
+});
+
+it("handles an error response RequestError", () => {
+  const action = {
+    payload: {
+      name: "RequestError",
+      message: "Hello Error",
+      status: 12345
+    }
+  };
+
+  const error = handleErrorResponse(action);
+  expect(error).toEqual({
+    message: "Dial backend is not currently available.",
+    statusCode: 31
+  });
+});
+
+it("handles an error response ApiError", () => {
+  const action = {
+    payload: {
+      name: "ApiError",
+      message: "Hello Error",
+      status: 12345
+    }
+  };
+
+  const error = handleErrorResponse(action);
+  expect(error).toEqual({ message: "Hello Error", statusCode: 12345 });
+});
+
+it("handles an error response UnknownError", () => {
+  const action = {
+    payload: {
+      name: "UnknownError",
+      message: "Hello Error",
+      status: 12345
+    }
+  };
+
+  const error = handleErrorResponse(action);
+  expect(error).toEqual({ message: "Hello Error", statusCode: -1 });
+});
+
+it("handles an unknown error with Token", () => {
+  let state = {};
+  const action = {
+    payload: {
+      name: "UnknownError",
+      message: "Hello Error",
+      status: 12345
+    }
+  };
+
+  const error = handleErrorWithToken(state, action);
+  expect(error).toEqual({ error: { message: "Hello Error", statusCode: -1 } });
+});
+
+it("handles an unknown error with Login", () => {
+  let state = {};
+  const action = {
+    payload: {
+      name: "UnknownError",
+      message: "Hello Error",
+      status: 12345
+    }
+  };
+
+  const error = handleErrorWithLogin(state, action);
+  expect(error).toEqual({
+    error: { message: "Hello Error", statusCode: -1 },
+    loginInProgress: false
+  });
+});
+
+it("handles a RequestError error with Login", () => {
+  let state = {};
+  const action = {
+    payload: {
+      name: "RequestError",
+      message: "Hello Error",
+      status: 12345
+    }
+  };
+
+  const error = handleErrorWithLogin(state, action);
+  expect(error).toEqual({
+    error: {
+      message:
+        "Currently It is not possible to log in. Please, try again in a few minutes.",
+      statusCode: 31
+    },
+    loginInProgress: false
+  });
+});
+
+it("returns error key from state", () => {
+  let state = {
+    error: true
+  };
+  const error = errors(state);
+  expect(error).toEqual(true);
 });
--- src/login/utils/errors.js
+++ src/login/utils/errors.js
@@ -1,69 +1,93 @@
-import { logMessage } from "common/utils";
+/**
+ * Creates a dict with error format
+ * @param message (string) Message that will be displayed
+ * @param statusCode (int) Error code
+ * @returns {{message: string, statusCode: number}}
+ */
+export const buildError = (message = "Unknown error", statusCode = 999) => {
+  return { message, statusCode };
+};
 
 /**
- * Handles the error with the token
+ * Creates a dict with error format from an Redux Api Middleware action
+ * @param action Redux API middleware action
+ * @returns {{message: *, statusCode: number}}
+ */
+export const buildApiErrorFromAction = action => {
+  const message = action.payload.message;
+  const statusCode = action.payload.status ? action.payload.status : -1;
+  return { message, statusCode };
+};
+
+/**
+ * Handles a given action as an error
+ * @param action RAM Action expected to be an error
+ * @param defaultErrorMessage (string) Default Message that will be displayed
+ * @returns {{message: string, statusCode: number}|{message: *, statusCode: number}}
+ */
+export const handleErrorResponse = (
+  action,
+  defaultErrorMessage = "Dial backend is not currently available."
+) => {
+  let response;
+  switch (action.payload.name) {
+    case "RequestError":
+      response = buildError(defaultErrorMessage, 31);
+      break;
+    case "ApiError":
+      response = buildApiErrorFromAction(action);
+      break;
+    default:
+      response = buildError(action.payload.message, -1);
+      break;
+  }
+  return response;
+};
+
+/**
+ *
  * @param state
  * @param action
  * @returns {{[p: string]: *}}
  */
-export function handleErrorWithToken (state, action) {
-  let message;
-  let statusCode;
-  logMessage(action);
-
-  if (action.payload.message) {
-    if (action.payload.name === "RequestError") {
-      message = "Dial backend is not currently available.";
-      statusCode = 31;
-    } else if (action.payload.name === "ApiError") {
-      message = action.payload.message;
-      statusCode = action.payload.status ? action.payload.status : -1;
-    } else {
-      message = action.payload.message;
-      statusCode = -1;
-    }
+export const handleErrorWithToken = (state, action) => {
+  let response;
+  if (!action.payload || !action.payload.message || !action.payload.name) {
+    response = buildError();
   } else {
-    message = "Unknown error";
-    statusCode = 999;
+    response = handleErrorResponse(action);
   }
+
   return {
     ...state,
-    error: { message: message, statusCode: statusCode }
+    error: response
   };
-}
+};
 
-export function handleErrorWithLogin (state, action) {
-  let message;
-  let statusCode;
-  logMessage(action);
-
-  if (action.payload.message) {
-    if (action.payload.name === "RequestError") {
-      message = "Currently It is not possible to log in. Please, try again in a few minutes.";
-      statusCode = 31;
-    } else if (action.payload.name === "ApiError") {
-      message = action.payload.message;
-      statusCode = action.payload.status ? action.payload.status : -1;
-    } else {
-      message = action.payload.message;
-      statusCode = -1;
-    }
+export const handleErrorWithLogin = (state, action) => {
+  let response;
+  if (!action.payload) {
+    response = buildError();
   } else {
-    message = "Unknown error";
-    statusCode = 999;
+    if (action.payload.message && action.payload.name) {
+      response = handleErrorResponse(
+        action,
+        "Currently It is not possible to log in. Please, try again in a few minutes."
+      );
+    }
   }
   return {
     ...state,
     loginInProgress: false,
-    error: { message: message, statusCode: statusCode }
+    error: response
   };
-}
+};
 
 /**
  * Gets the errors from the state
  * @param state An state dict
  * @returns {*} A dict of errors
  */
-export function errors (state) {
+export const errors = state => {
   return state.error;
-}
+};
