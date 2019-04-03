483ea134ae3fe8219c2570c5f4283bbeec353eef
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-04-03 12:11:27 +0200

    Refactor DtmfDialpadForm

--- src/calls/components/dialpads/DtmfDialpadForm/DtmfDialpadForm.js
+++ src/calls/components/dialpads/DtmfDialpadForm/DtmfDialpadForm.js
@@ -5,6 +5,27 @@ import { logMessage } from "common/utils/logs";
 import PropTypes from "prop-types";
 import { phoneService } from "calls/providers/PhoneProvider/PhoneProvider";
 
+function DtmfDialpadField(props) {
+  return (
+    <Form>
+      <Form.Field width={16}>
+        <Input
+          className={"DialpadInput"}
+          value={props.value}
+          placeholder={"Input a dtmf command..."}
+          onChange={props.onChange}
+          icon={<Icon name="text telephone" inverted color={"blue"} circular />}
+        />
+      </Form.Field>
+    </Form>
+  );
+}
+
+DtmfDialpadField.propTypes = {
+  value: PropTypes.string,
+  onChange: PropTypes.func
+};
+
 export class DtmfDialpadForm extends Component {
   static propTypes = {
     phoneService: PropTypes.object.isRequired // Phone Service
@@ -23,11 +44,11 @@ export class DtmfDialpadForm extends Component {
   };
 
   sendDtmf = () => {
-    const {phoneService} = this.props;
-    const {dialpadValue} = this.state;
+    const { phoneService } = this.props;
+    const { dialpadValue } = this.state;
     logMessage("Sending DTMF");
     phoneService.sendDtmfCommand(dialpadValue);
-    this.setState({dialpadValue: ''})
+    this.setState({ dialpadValue: "" });
   };
 
   render() {
@@ -35,24 +56,10 @@ export class DtmfDialpadForm extends Component {
       <Grid>
         <Grid.Row>
           <Grid.Column width={16}>
-            <Form>
-              <Form.Field width={16}>
-                <Input
-                  className={"DialpadInput"}
-                  value={this.state.dialpadValue}
-                  placeholder={"Input a dtmf command..."}
-                  onChange={this.handleChange}
-                  icon={
-                    <Icon
-                      name="text telephone"
-                      inverted
-                      color={"blue"}
-                      circular
-                    />
-                  }
-                />
-              </Form.Field>
-            </Form>
+            <DtmfDialpadField
+              value={this.state.dialpadValue}
+              onChange={this.handleChange}
+            />
           </Grid.Column>
         </Grid.Row>
         <Grid.Row>
@@ -68,7 +75,7 @@ export class DtmfDialpadForm extends Component {
   }
 }
 
-export default phoneService(DtmfDialpadForm)
+export default phoneService(DtmfDialpadForm);
 //
 // DtmfDialpadForm.propTypes = {
 //   value: PropTypes.any,
