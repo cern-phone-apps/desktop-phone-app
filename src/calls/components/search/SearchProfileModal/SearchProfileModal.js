ad77e6abaa9e399c399823a8725e51b72d7d6f9a
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-04-03 10:20:59 +0200

    Refactor SearchProfileModal

--- src/calls/components/search/SearchProfileModal/SearchProfileModal.js
+++ src/calls/components/search/SearchProfileModal/SearchProfileModal.js
@@ -15,6 +15,61 @@ import ContactAddButtonContainer from "calls/components/contacts/ContactAddButto
 import { UserProfileExtraInfo } from "calls/components/UserProfileExtraInfo/UserProfileExtraInfo";
 
 
+function SearchProfileModalHeader (props) {
+  return <Header>
+    <Icon name="user" color={"blue"}/>
+    <Header.Content>
+      <Header as="h5" floated={"right"}>
+        <ContactAddButtonContainer contact={props.contact}/>
+      </Header>
+      {props.contact ? props.contact.displayName : ""}
+      <Header.Subheader>
+        {props.contact ? formatUserOrganization(props.contact) : ""}
+      </Header.Subheader>
+    </Header.Content>
+  </Header>;
+}
+
+SearchProfileModalHeader.propTypes = { contact: PropTypes.any };
+
+function SearchProfileModalContent (props) {
+  return <Modal.Content>
+    {props.user && (
+      <UserProfileExtraInfo
+        mail={props.user.mail}
+        physicalDeliveryOfficeName={
+          props.user.physicalDeliveryOfficeName
+        }
+      />
+    )}
+    {!props.user ? (
+      <Segment basic>
+        <Dimmer active inverted>
+          <Loader inverted size={"large"}/>
+        </Dimmer>
+      </Segment>
+    ) : (
+      props.user.phones.map((phone, index) => {
+        if (phone.number !== null) {
+          return (
+            <UserPhoneNumberButtonContainer
+              key={`button-${index}`}
+              phoneNumber={phone.number}
+              icon={phone.phoneType}
+              recipientName={props.user.displayName}
+            />
+          );
+        } else return null;
+      })
+    )}
+  </Modal.Content>;
+}
+
+SearchProfileModalContent.propTypes = {
+  user: PropTypes.any,
+  f: PropTypes.func
+};
+
 export class SearchProfileModal extends Component {
   static propTypes = {
     user: PropTypes.object,
@@ -37,48 +92,8 @@ export class SearchProfileModal extends Component {
         onClose={this.handleClose}
         closeIcon
       >
-        <Header>
-          <Icon name="user" color={"blue"} />
-          <Header.Content>
-            <Header as="h5" floated={"right"}>
-              <ContactAddButtonContainer contact={user} />
-            </Header>
-            {user ? user.displayName : ""}
-            <Header.Subheader>
-              {user ? formatUserOrganization(user) : ""}
-            </Header.Subheader>
-          </Header.Content>
-        </Header>
-        <Modal.Content>
-          {user && (
-            <UserProfileExtraInfo
-              mail={user.mail}
-              physicalDeliveryOfficeName={
-                user.physicalDeliveryOfficeName
-              }
-            />
-          )}
-          {!user ? (
-            <Segment basic>
-              <Dimmer active inverted>
-                <Loader inverted size={"large"} />
-              </Dimmer>
-            </Segment>
-          ) : (
-            user.phones.map((phone, index) => {
-              if (phone.number !== null) {
-                return (
-                  <UserPhoneNumberButtonContainer
-                    key={`button-${index}`}
-                    phoneNumber={phone.number}
-                    icon={phone.phoneType}
-                    recipientName={user.displayName}
-                  />
-                );
-              } else return null;
-            })
-          )}
-        </Modal.Content>
+        <SearchProfileModalHeader contact={user}/>
+        <SearchProfileModalContent user={user}/>
       </Modal>
     );
   }
