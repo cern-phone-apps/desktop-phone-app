1d7c8fe1a9696601ad5bbde1b57574414fc8af94
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-05-22 09:24:51 +0200

    Make search work on mobile

--- mobile/src/calls/screens/SearchUsersScreen/SearchUsersScreenContainer.js
+++ mobile/src/calls/screens/SearchUsersScreen/SearchUsersScreenContainer.js
@@ -7,20 +7,28 @@ import { usersActionFactory, contactsActionFactory } from 'dial-core';
 import SearchUsersScreen from './SearchUsersScreen';
 import withOnGoingCallBanner from '../../../common/utils/calls';
 
-const usersActions = usersActionFactory(API_ENDPOINT);
+function mapStateToProps(state) {
+  const { calls } = state;
+  return {
+    searching: calls.search.serching,
+    contacts: calls.contacts.getContacts.contacts
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators(
+    {
+      searchUsers: usersActionFactory(API_ENDPOINT, 'mobile').searchUsers,
+      addUserContact: contactsActionFactory(API_ENDPOINT, 'mobile')
+        .addUserContact,
+      getUserContacts: contactsActionFactory(API_ENDPOINT, 'mobile')
+        .getUserContacts
+    },
+    dispatch
+  );
+}
 
 export default connect(
-  ({ users, contacts: { contacts } }) => ({
-    ...users,
-    contacts
-  }),
-  dispatch =>
-    bindActionCreators(
-      {
-        ...usersActions,
-        addUserContact: contactsActionFactory(API_ENDPOINT).addUserContact,
-        getUserContacts: usersActionFactory(API_ENDPOINT).getUserContacts
-      },
-      dispatch
-    )
+  mapStateToProps,
+  mapDispatchToProps
 )(withOnGoingCallBanner(SearchUsersScreen));
