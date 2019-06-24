a878be274f0faf8e94240bb69520f075317e7635
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-06-24 10:36:22 +0200

    Add react native callkeep

--- mobile/android/app/src/main/java/ch/cern/dialmobile/MainApplication.java
+++ mobile/android/app/src/main/java/ch/cern/dialmobile/MainApplication.java
@@ -3,6 +3,7 @@ package ch.cern.dialmobile;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import io.wazo.callkeep.RNCallKeepPackage;
 import com.zmxv.RNSound.RNSoundPackage;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -29,6 +30,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNCallKeepPackage(),
             new RNSoundPackage(),
             new RNCWebViewPackage(),
             new VectorIconsPackage(),
