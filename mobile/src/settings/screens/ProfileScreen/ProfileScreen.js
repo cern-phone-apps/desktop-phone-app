70b47a98f2ca4b4e2b1f667153d008a6c0a7a68c
Pablo Roncero Fernández <pablo.roncero.fernandez@cern.ch>
2019-06-27 16:54:25 +0200

    Add profile to settings screen

---
+++ mobile/src/settings/screens/ProfileScreen/ProfileScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View } from 'react-native';
+import ProfileContainer from '../../components/profile/ProfileContainer';
+
+const ProfileScreen = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <ProfileContainer />
+    </View>
+  );
+};
+
+export default ProfileScreen;
