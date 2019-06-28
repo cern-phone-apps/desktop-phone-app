6671502683ad71fd7ef94160c5a71ba3754d3a2a
Pedro Ferreira <pedro.ferreira@cern.ch>
2019-06-28 09:47:50 +0200

    Make list of contacts/recent calls nicer

--- mobile/src/styles/ColorPalette.js
+++ mobile/src/styles/ColorPalette.js
@@ -3,5 +3,7 @@ export default {
   primaryLight: '#5c7ed0',
   primaryDark: '#002a6f',
   callBtnGreen: '#0fd859',
+  recentCallRed: '#e53935',
+  recentCallGreen: '#43a047',
   menuActive: '#ffffff'
 };
