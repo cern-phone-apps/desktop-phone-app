d5a46b17761be2257edd1b2fa9724a0460e261d1
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-07-01 11:30:15 +0200

    Display certificates message

--- webapp/src/calls/screens/CallsScreen/NotConnectedScreen/NotConnectedScreen.js
+++ webapp/src/calls/screens/CallsScreen/NotConnectedScreen/NotConnectedScreen.js
@@ -1,7 +1,8 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { translate } from 'react-i18next';
-import { Grid, Header, Segment, Modal } from 'semantic-ui-react';
+import { Grid, Header, Segment, Modal, Message } from 'semantic-ui-react';
+
 import RightColumn from 'common/components/RightColumn/RightColumn';
 import ErrorBoundary from 'common/components/ErrorBoundary/ErrorBoundary';
 import MainHeader from 'calls/components/MainHeader';
@@ -9,56 +10,74 @@ import ErrorMessageContainer from 'common/components/ErrorMessage/ErrorMessageCo
 import LogoutButtonContainer from 'auth/components/LogoutButton/LogoutButtonContainer';
 import NumberConnectorContainer from 'calls/components/NumberConnector/NumberConnectorContainer';
 
-export class SelectPhoneNumberModal extends Component {
-  render() {
-    return (
-      <Modal
-        open={this.props.modalOpen}
-        size="small"
-        className={'SelectPhoneModal'}
-      >
-        <Header icon="phone" content="Select a phone number" />
-        <Modal.Content>
-          <ErrorMessageContainer />
-          <p>
-            Select which one of your phone numbers you want to use with this
-            client.
-          </p>
-          <NumberConnectorContainer />
-          <hr />
-          <LogoutButtonContainer color="red" />
-        </Modal.Content>
-      </Modal>
-    );
-  }
+function SelectPhoneNumberModal({ modalOpen }) {
+  const getCERNCertificatesURL = () =>
+    'https://cafiles.cern.ch/cafiles/certificates/Grid.aspx';
+
+  return (
+    <Modal open={modalOpen} size="small" className="SelectPhoneModal">
+      <Header icon="phone" content="Select a phone number" />
+      <Modal.Content>
+        <Message
+          info
+          icon="certificate"
+          header="Have you installed CERN certificates?"
+          content={
+            <p>
+              CERN certificates are required to run this application. If you
+              haven&apos;t intelled them yet, please follow the steps{' '}
+              <a
+                href={getCERNCertificatesURL()}
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                in this URL
+              </a>
+              .
+            </p>
+          }
+        />
+        <ErrorMessageContainer />
+        <p>
+          Select which one of your phone numbers you want to use with this
+          client.
+        </p>
+        <NumberConnectorContainer />
+        <hr />
+        <LogoutButtonContainer color="red" />
+      </Modal.Content>
+    </Modal>
+  );
 }
 
-export class NotConnectedScreen extends Component {
-  static propTypes = {
-    t: PropTypes.func.isRequired
-  };
+SelectPhoneNumberModal.propTypes = {
+  modalOpen: PropTypes.bool.isRequired
+};
 
-  render() {
-    return (
-      <Grid.Column
-        computer={12}
-        mobile={16}
-        tablet={16}
-        className="CallsScreen__RightColumn"
-      >
-        <RightColumn>
-          <MainHeader />
-          <ErrorBoundary>
-            <div className="call-inner-content">
-              <Segment textAlign="center" basic>
-                <SelectPhoneNumberModal modalOpen />
-              </Segment>
-            </div>
-          </ErrorBoundary>
-        </RightColumn>
-      </Grid.Column>
-    );
-  }
+function NotConnectedScreen({ t }) {
+  return (
+    <Grid.Column
+      computer={12}
+      mobile={16}
+      tablet={16}
+      className="CallsScreen__RightColumn"
+    >
+      <RightColumn>
+        <MainHeader />
+        <ErrorBoundary>
+          <div className="call-inner-content">
+            <Segment textAlign="center" basic>
+              <SelectPhoneNumberModal modalOpen />
+            </Segment>
+          </div>
+        </ErrorBoundary>
+      </RightColumn>
+    </Grid.Column>
+  );
 }
+
+NotConnectedScreen.propTypes = {
+  t: PropTypes.func.isRequired
+};
 
 export default translate('calls')(NotConnectedScreen);
