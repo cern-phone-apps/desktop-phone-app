62492eb8cdd4c9150a0fd60a257761d05ab61071
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-07-10 09:46:20 +0200

    Display active phone number in the header (#13)

--- webapp/src/calls/screens/CallsScreen/OnCallScreen/OnCallScreen.js
+++ webapp/src/calls/screens/CallsScreen/OnCallScreen/OnCallScreen.js
@@ -1,17 +1,17 @@
-import React, { Component } from "react";
-import { Grid, Segment } from "semantic-ui-react";
-import OnCallDetailsContainer from "calls/components/OnCallDetails/OnCallDetailsContainer";
-import RightColumn from "common/components/RightColumn/RightColumn";
-import ErrorBoundary from "common/components/ErrorBoundary/ErrorBoundary";
-import MainHeader from "calls/components/MainHeader";
-import DtmfDialpadForm from "calls/components/dialpads/DtmfDialpadForm/DtmfDialpadForm";
-import styles from "calls/screens/CallsScreen/ConnectedScreen/ConnectedScreen.module.css";
+import React, { Component } from 'react';
+import { Grid, Segment } from 'semantic-ui-react';
+import OnCallDetailsContainer from 'calls/components/OnCallDetails/OnCallDetailsContainer';
+import RightColumn from 'common/components/RightColumn/RightColumn';
+import ErrorBoundary from 'common/components/ErrorBoundary/ErrorBoundary';
+import MainHeaderContainer from 'calls/components/MainHeader/MainHeaderContainer';
+import DtmfDialpadForm from 'calls/components/dialpads/DtmfDialpadForm/DtmfDialpadForm';
+import styles from 'calls/screens/CallsScreen/ConnectedScreen/ConnectedScreen.module.css';
 import IncomingCallModalContainer from 'calls/components/call_modals/IncomingCallModal/IncomingCallModalContainer';
 
-
 export class OnCallScreen extends Component {
-  styles = { height: "100%" };
-  dividedStyles = { paddingTop: "0", paddingBottom: 0 };
+  styles = { height: '100%' };
+
+  dividedStyles = { paddingTop: '0', paddingBottom: 0 };
 
   render() {
     return (
@@ -19,11 +19,11 @@ export class OnCallScreen extends Component {
         computer={12}
         mobile={16}
         tablet={16}
-        className={"CallsScreen__RightColumn"}
+        className="CallsScreen__RightColumn"
       >
         <RightColumn>
           <div className={styles.innerContainer}>
-            <MainHeader />
+            <MainHeaderContainer />
             <IncomingCallModalContainer />
 
             {this.renderMainContent()}
