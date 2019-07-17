874c06c60033ad4e269e63d46450059894dabd96
lopez <llopez@student.42.fr>
2019-07-17 08:48:38 +0200

    Fix calling date

--- webapp/src/calls/components/recent_calls/RecentCallList/RecentCallList.js
+++ webapp/src/calls/components/recent_calls/RecentCallList/RecentCallList.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import { Item } from "semantic-ui-react";
 import RecentCall from "calls/components/recent_calls/RecentCall/RecentCall";
 import ScrollableContent from "common/components/ScrollableContent/ScrollableContent";
+import RecentCallModal from "calls/components/recent_calls/RecentCallModal/RecentCallModal.js"
 
 /**
  * Displays a scrollable list of RecentCall Components
@@ -20,7 +21,16 @@ class RecentCallList extends Component {
       <ScrollableContent>
         <Item.Group link>
           {recentCalls.map((item, index) => {
-            return <RecentCall key={`recent-${index}`} recentCall={item} />;
+            return (
+              <RecentCallModal key={`recent-${index}`} recentCall={item}
+              size="tiny"
+              dimmer="blurring"
+              closeIcon
+              trigger={
+                <RecentCall key={`recent-${index}`} recentCall={item}/>
+              }
+            />
+            );
           })}
         </Item.Group>
       </ScrollableContent>
