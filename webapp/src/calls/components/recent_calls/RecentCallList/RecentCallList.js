0e190e790199617b6b43113f899cc35210b910b6
lopez <llopez@student.42.fr>
2019-07-17 09:13:32 +0200

    checkout recentcalllist (added by mistake)

--- webapp/src/calls/components/recent_calls/RecentCallList/RecentCallList.js
+++ webapp/src/calls/components/recent_calls/RecentCallList/RecentCallList.js
@@ -4,7 +4,6 @@ import PropTypes from "prop-types";
 import { Item } from "semantic-ui-react";
 import RecentCall from "calls/components/recent_calls/RecentCall/RecentCall";
 import ScrollableContent from "common/components/ScrollableContent/ScrollableContent";
-import RecentCallModal from "calls/components/recent_calls/RecentCallModal/RecentCallModal.js"
 
 /**
  * Displays a scrollable list of RecentCall Components
@@ -21,16 +20,7 @@ class RecentCallList extends Component {
       <ScrollableContent>
         <Item.Group link>
           {recentCalls.map((item, index) => {
-            return (
-              <RecentCallModal key={`recent-${index}`} recentCall={item}
-              size="tiny"
-              dimmer="blurring"
-              closeIcon
-              trigger={
-                <RecentCall key={`recent-${index}`} recentCall={item}/>
-              }
-            />
-            );
+            return <RecentCall key={`recent-${index}`} recentCall={item} />;
           })}
         </Item.Group>
       </ScrollableContent>
