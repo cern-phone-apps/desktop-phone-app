39560356c0ee68b179740a624802c0ffc2dbe271
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-07-30 14:58:47 +0200

    Add removeContacts reducer

--- core/src/reducers/contacts/index.js
+++ core/src/reducers/contacts/index.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux';
 import getContacts from './getContacts';
 import addContacts from './addContacts';
+import removeContacts from './removeContacts';
 import modal from './modal';
 import emergencyModal from './emergencyModal';
 
 const contactsReducer = combineReducers({
   getContacts,
   addContacts,
+  removeContacts,
   modal,
   emergencyModal
 });
