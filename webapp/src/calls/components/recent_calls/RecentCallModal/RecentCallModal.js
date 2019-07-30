3508387f8131d59ae500e8bd826a3240db268e10
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-07-30 15:05:45 +0200

    Refactor RecentCallModal

--- webapp/src/calls/components/recent_calls/RecentCallModal/RecentCallModal.js
+++ webapp/src/calls/components/recent_calls/RecentCallModal/RecentCallModal.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
-import { Modal, Header, Icon } from 'semantic-ui-react';
-import moment from "moment";
+import { Modal, Icon } from 'semantic-ui-react';
+import moment from 'moment';
 import UserPhoneNumberButtonContainer from 'calls/components/UserPhoneNumberButton/UserPhoneNumberButtonContainer';
+
+function RecentCallDuration({ endTime, startTime }) {
+  if (endTime && startTime) {
+    return (
+      <li style={{ margin: 10 }}>
+        <Icon name="stopwatch" />
+        {moment.duration(moment(endTime).diff(moment(startTime))).humanize()}
+      </li>
+    );
+  }
+  return null;
+}
+
+function RecentCallTime({ endTime, incoming, missed }) {
+  return (
+    <li style={{ margin: 10 }}>
+      <Icon
+        name={incoming ? 'arrow down' : 'arrow up'}
+        color={missed ? 'red' : 'green'}
+      />
+      {moment(endTime).calendar()}
+    </li>
+  );
+}
 
 export class RecentCallModal extends Component {
   inlineStyle = {
@@ -11,18 +35,6 @@ export class RecentCallModal extends Component {
       marginRight: 'auto'
     }
   };
-  
-  getDuration(endTime, startTime) {
-    if (endTime && startTime)
-      return <li style={{ margin: 10 }}><Icon name="stopwatch"/>{moment.duration(moment(endTime).diff(moment(startTime))).humanize()}</li>;          
-  }
-
-  getTime(endTime, incoming, missed) {
-    let Way;
-
-    Way = <Icon name={(incoming) ? "arrow down" : "arrow up"} color={(missed) ? "red" : "green"}/>;
-    return (<li style={{ margin: 10 }}>{Way}{moment(endTime).calendar()}</li>);
-  }
 
   render() {
     const {
@@ -34,23 +46,30 @@ export class RecentCallModal extends Component {
       incoming
     } = this.props.recentCall;
     const { trigger } = this.props;
-    
+
     return (
-      <Modal size="tiny" trigger={trigger} key={name+"-"+endTime+"-modal"}>
+      <Modal size="tiny" trigger={trigger} key={`${name}-${endTime}-modal`}>
         <Modal.Header>
-      <Icon name="phone" color="blue" /> Call details
+          <Icon name="phone" color="blue" /> Call details
         </Modal.Header>
         <Modal.Content>
           <Modal.Description>
             <Modal.Header></Modal.Header>
-            <li style={{ margin: 10 }}><Icon name="phone"/>{phoneNumber}</li>
-            {this.getDuration(endTime, startTime)}
-            {this.getTime(endTime, incoming, missed)}
+            <li style={{ margin: 10 }}>
+              <Icon name="phone" />
+              {phoneNumber}
+            </li>
+            <RecentCallDuration endTime={endTime} startTime={startTime} />
+            <RecentCallTime
+              endTime={endTime}
+              incoming={incoming}
+              missed={missed}
+            />
           </Modal.Description>
           <UserPhoneNumberButtonContainer
-                phoneNumber={phoneNumber}
-                icon="phone"
-              />
+            phoneNumber={phoneNumber}
+            icon="phone"
+          />
         </Modal.Content>
       </Modal>
     );
