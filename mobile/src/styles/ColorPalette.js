295c2e653769c194d3a0b25570f23b800ece70db
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-08-02 09:46:21 +0200

    Extract mobile package

--- mobile/src/styles/ColorPalette.js
+++
@@ -1,9 +0,0 @@
-export default {
-  primary: '#22529e',
-  primaryLight: '#5c7ed0',
-  primaryDark: '#002a6f',
-  callBtnGreen: '#0fd859',
-  recentCallRed: '#e53935',
-  recentCallGreen: '#43a047',
-  menuActive: '#ffffff'
-};
