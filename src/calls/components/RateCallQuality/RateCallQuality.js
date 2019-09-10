f7c1b5ce0a053e46f58dd239a46899dc5cec7e78
lopez <llopez@student.42.fr>
2019-09-10 08:45:28 +0200

    Add two buttons to rating modal (close and send) next : send rate to backend

--- src/calls/components/RateCallQuality/RateCallQuality.js
+++ src/calls/components/RateCallQuality/RateCallQuality.js
@@ -1,27 +1,38 @@
 import React, { useState } from 'react';
-import { Button, Header, Modal, Icon, Rating } from "semantic-ui-react";
-
-const onRate = (value) => {
-  alert("only "+value+'??');
-        // Send rate value to backend
-};
+import { Button, Header, Modal, Icon, Rating } from 'semantic-ui-react';
 
 const RateCallQuality = ({ lastCall }) => {
-  const [ rate, setRate ] = useState(true);
-  if (lastCall[0].endTime > new Date().getTime() - 10000000000)
-    return (
-      <Modal
-        open={rate}
-        size="small"
-        //className={"OutgoingCallModal"}
-      >
-        <Header icon="star" content="Call quality feedback" />
-        <Modal.Content>
-          <Rating maxRating={5} defaultRating={3} icon='star' size='massive' onRate={(e) => setRate(false)}/>
-        </Modal.Content>
-      </Modal>
-    );
-  return null;
+  const [displayModal, setDisplayModal] = useState(
+    lastCall[0].endTime > new Date().getTime() - 10000
+  );
+  const [rate, setRate] = useState(0);
+  const onRate = value => {
+    // Send rating to backend here
+    setDisplayModal(false);
+  };
+  return (
+    <Modal open={displayModal} size="small">
+      <Header icon="star" content="Call quality feedback" />
+      <Modal.Content>
+        <Rating
+          maxRating={5}
+          icon="star"
+          size="massive"
+          onRate={(e, { rating }) => {
+            setRate(rating);
+          }}
+        />
+      </Modal.Content>
+      <Modal.Actions>
+        <Button color="red" onClick={() => setDisplayModal(false)}>
+          Close
+        </Button>
+        <Button color="green" onClick={() => onRate(rate)}>
+          Send
+        </Button>
+      </Modal.Actions>
+    </Modal>
+  );
 };
 
 export default RateCallQuality;
