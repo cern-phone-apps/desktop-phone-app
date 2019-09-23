f3ec1dbc2508a0c9609e530f12928ab225d6a78b
lopez <llopez@student.42.fr>
2019-09-23 13:31:08 +0200

    Check everytime every devices

--- src/calls/components/CallButton/CallButtonElement.js
+++ src/calls/components/CallButton/CallButtonElement.js
@@ -10,19 +10,15 @@ import DetectRTC from 'detectrtc';
 
 export function CallButtonElement({ text, onClick }) {
   function checkDevices() {
-    if (DetectRTC.hasSpeakers && DetectRTC.hasMicrophone) {
-      onClick();
-    } else {
-      DetectRTC.load(() => {
-        if (DetectRTC.hasSpeakers && DetectRTC.hasMicrophone) {
-          onClick();
-        } else {
-          alert(
-            'There are no input/output devices.\nPlease connect at least one speaker and one microphone.'
-          );
-        }
-      });
-    }
+    DetectRTC.load(() => {
+      if (DetectRTC.hasSpeakers && DetectRTC.hasMicrophone) {
+        onClick();
+      } else {
+        alert(
+          'There are no input/output devices.\nPlease connect at least one speaker and one microphone.'
+        );
+      }
+    });
   }
 
   return <div onClick={checkDevices}>{text}</div>;
