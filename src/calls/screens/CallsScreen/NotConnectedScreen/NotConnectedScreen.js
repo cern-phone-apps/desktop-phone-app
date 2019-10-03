81e0e56ccbf42193c123e8380db3e427f1a6ddc6
lopez <llopez@student.42.fr>
2019-10-03 09:32:30 +0200

    register screen is a modal now new command in package.json to only-pack-windows

--- src/calls/screens/CallsScreen/NotConnectedScreen/NotConnectedScreen.js
+++ src/calls/screens/CallsScreen/NotConnectedScreen/NotConnectedScreen.js
@@ -1,11 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { translate } from 'react-i18next';
-import {
-  Header,
-  Message,
-  Divider
-} from 'semantic-ui-react';
+import { Header, Modal, Divider } from 'semantic-ui-react';
 import { Redirect } from 'react-router-dom';
 
 import ErrorBoundary from 'common/components/ErrorBoundary/ErrorBoundary';
@@ -17,44 +13,10 @@ import SettingsButtonContainer from 'common/components/SettingsButton/SettingsBu
 import SettingsModalContainer from 'settings/components/SettingsModal/SettingsModalContainer';
 
 function SelectPhoneNumberModal() {
-  const getCERNCertificatesURL = () =>
-    'https://cafiles.cern.ch/cafiles/certificates/Grid.aspx';
-
   return (
-    <div
-      style={{
-        flex: 4,
-        backgroundColor: 'white',
-        padding: '5%',
-        margin: 'auto',
-        display: 'inline-block',
-        width: '100%',
-        height: '100%',
-        alignItems: 'center'
-      }}
-    >
+    <Modal open={true} size="small" className="SelectPhoneModal">
       <Header icon="phone" content="Select a phone number" />
-      <Divider clearing />
-      <div>
-        <Message
-          info
-          icon="certificate"
-          header="Have you installed CERN certificates?"
-          content={
-            <p>
-              CERN CA certificates are required to run this application. If you
-              haven&apos;t installed them yet, please follow the steps{' '}
-              <a
-                href={getCERNCertificatesURL()}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                in this URL
-              </a>
-              .
-            </p>
-          }
-        />
+      <Modal.Content>
         <ErrorMessageContainer />
         <p>
           Select which one of your phone numbers you want to use with this
@@ -65,8 +27,8 @@ function SelectPhoneNumberModal() {
         <DownloadDebugLogsButton floated="right" />
         <SettingsButtonContainer floated="right" />
         <LogoutButtonContainer color="red" />
-      </div>
-    </div>
+      </Modal.Content>
+    </Modal>
   );
 }
 
@@ -75,10 +37,8 @@ SelectPhoneNumberModal.propTypes = {
 };
 
 function NotConnectedScreen({ isAuthenticated, connected }) {
-  if (!isAuthenticated)
-    return <Redirect to='/login'/>
-  if (connected)
-    return <Redirect to='/home'/>
+  if (!isAuthenticated) return <Redirect to="/login" />;
+  if (connected) return <Redirect to="/home" />;
   return (
     <ErrorBoundary>
       <SettingsModalContainer />
