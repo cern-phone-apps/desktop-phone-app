b98bcdcdc1567858c8f0c3e3a32d540d20741d90
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-10-07 11:29:42 +0200

    Add test and snapshot for UserProfileExtraInfo

---
+++ src/calls/components/UserProfileExtraInfo/UserProfileExtraInfo.test.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { render, waitForElement } from '@testing-library/react';
+import UserProfileExtraInfo from './UserProfileExtraInfo';
+
+global.remote = {
+  require: jest.fn()
+};
+
+describe('UserProfileExtraInfo', () => {
+  it('renders the component', () => {
+    const { container } = render(
+      <UserProfileExtraInfo
+        mail="johndoe1@cern.ch"
+        physicalDeliveryOfficeName="28/1-002"
+      />
+    );
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  it('renders component and includes text', async () => {
+    const { getByText } = render(
+      <UserProfileExtraInfo
+        mail="johndoe@cern.ch"
+        physicalDeliveryOfficeName="28/1-002"
+      />
+    );
+
+    await waitForElement(() => getByText(/johndoe@cern.ch/i));
+    await waitForElement(() => getByText('28/1-002'));
+  });
+});
