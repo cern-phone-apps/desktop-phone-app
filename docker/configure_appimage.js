b30b9b29f668024c67367204608e3c19e5427252
lopez <llopez@student.42.fr>
2019-10-14 16:12:56 +0200

    Can push a new release with -no-sandbox by default completely resolve #141

---
+++ docker/configure_appimage.js
@@ -0,0 +1,9 @@
+const system = require("system-commands");
+
+exports.default = async function(context) {
+  console.log("Adding --no-sandox to AppImage");
+  system("docker build docker/");
+  system("docker run -v $(pwd)/dist/:/root/home/ --privileged $(docker images -a | awk '{ print $3}' | awk 'NR==2')");
+  console.log("Done.");
+  return [];
+}
