b352bb6dc791dba309ebb396949e60b58223e0ef
Lorys Lopez <llopez@iMac-de-Lorys.local>
2019-10-15 15:05:44 +0200

    change the way it gets the appImage file. Now it's taking the latest matching file.

--- docker/bash.sh
+++ docker/bash.sh
@@ -1,8 +1,8 @@
 cd /root/home
 wget "https://github.com/AppImage/AppImageKit/releases/download/12/appimagetool-x86_64.AppImage"
 chmod +x appimagetool-x86_64.AppImage
-./cern-phone-app-$(ls cern-phone-app-*-x86_64-linux.AppImage | cut -d "-" -f 4)-x86_64-linux.AppImage --appimage-extract
+./cern-phone-app-$(ls -Art cern-phone-app-*-x86-64-linux.AppImage | tail -n 1 | cut -d "-" -f 4)-x86_64-linux.AppImage --appimage-extract
 sed -i -e 's|"$BIN"|"$BIN" --no-sandbox|g' squashfs-root/AppRun
 sed -i -e 's|^Icon=.*|Icon=cern-phone-app|g'  squashfs-root/cern-phone-app.desktop
 ./appimagetool-x86_64.AppImage squashfs-root/
-mv CERN_Phone_App-x86_64.AppImage cern-phone-app-$(ls cern-phone-app-*-x86_64-linux.AppImage | cut -d "-" -f 4)-x86_64-linux.AppImage
+mv CERN_Phone_App-x86_64.AppImage cern-phone-app-$(ls -Art cern-phone-app-*-x86-64-linux.AppImage | tail -n 1 | cut -d "-" -f 4)-x86_64-linux.AppImage
