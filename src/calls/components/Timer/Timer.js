1afb668790c64235f5ec336c4ac89ea35c104de6
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-10-16 10:21:25 +0200

    Refactor Timer into a function and add tests

--- src/calls/components/Timer/Timer.js
+++ src/calls/components/Timer/Timer.js
@@ -1,82 +1,72 @@
-import React, { Component } from "react";
-import PropTypes from "prop-types";
+import React, { useEffect, useState } from 'react';
+import PropTypes from 'prop-types';
 
-class Timer extends Component {
-  static propTypes = {
-    startTime: PropTypes.number,
-    countDown: PropTypes.bool,
-    className: PropTypes.string
-  };
+/**
+ * Displays a timer from an specified start time
+ * @param {startTime} number The start time of the timer (I.e. Date.now())
+ * @param {className} string Additional class applied to the component
+ */
+function Timer({ startTime, className }) {
+  const [time, setTime] = useState(0);
 
-  state = {
-    now: 0
-  };
-
-  static defaultProps = {
-    startTime: Date.now(),
-    className: "timer",
-    countDown: null
-  };
-
-  constructor(props) {
-    super();
-    if (props.startTime > Date.now() && !props.countDown) {
-      throw Error("If startTime is in the future, countDown must be true");
-    }
-  }
-
-  componentWillMount() {
-    this.tick();
-    this.timer = setInterval(this.tick, 1000);
-  }
-
-  componentWillUnmount() {
-    clearInterval(this.timer);
-  }
-
-  tick = () => {
-    this.props.countDown
-      ? this.setState({ time: this.props.startTime - Date.now() })
-      : this.setState({ time: Date.now() - this.props.startTime });
-  };
-
-  convertTimeToString = () => {
-    return `${this.convertTimeToMinutesString()}:${this.convertTimeToSecondsString()}`;
-  };
-
-  convertTimeToMinutesString = () => {
-    let minutes = this.convertTimeToMinutes();
-    minutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
-    return `${minutes}`;
-  };
-
-  convertTimeToSecondsString = () => {
-    let seconds = this.convertTimeToSeconds();
-    seconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
-    return `${seconds}`;
-  };
-
-  convertTimeToSeconds = () => {
-    let ms = this.state.time;
-    const seconds = Math.round(ms / 1000) % 60;
-    return seconds;
-  };
-
-  convertTimeToMinutes = () => {
-    const seconds = Math.round(this.state.time / 1000);
-    let minutes = Math.floor(seconds / 60);
+  const convertTimeToMinutes = () => {
+    const seconds = Math.round(time / 1000);
+    const minutes = Math.floor(seconds / 60);
 
     return minutes;
   };
 
-  render() {
-    return (
-      <div className={this.props.className}>
-        <span className={"minutes"}>{this.convertTimeToMinutesString()}</span>:
-        <span className={"seconds"}>{this.convertTimeToSecondsString()}</span>
-      </div>
-    );
-  }
+  const convertTimeToSeconds = () => {
+    const ms = time;
+    const seconds = Math.round(ms / 1000) % 60;
+    return seconds;
+  };
+
+  const convertTimeToMinutesString = () => {
+    let minutes = convertTimeToMinutes();
+    minutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+    return `${minutes}`;
+  };
+
+  const convertTimeToSecondsString = () => {
+    let seconds = convertTimeToSeconds();
+    seconds = seconds < 10 ? `0${seconds}` : `${seconds}`;
+    return `${seconds}`;
+  };
+
+  useEffect(() => {
+    const tick = () => {
+      setTime(Date.now() - startTime);
+    };
+
+    tick();
+    const timer = setInterval(tick, 1000);
+    return () => {
+      clearInterval(timer);
+    };
+  }, [startTime]);
+
+  return (
+    <div className={className}>
+      <span className="minutes" aria-label="Minutes">
+        {convertTimeToMinutesString()}
+      </span>
+      :
+      <span className="seconds" aria-label="Seconds">
+        {convertTimeToSecondsString()}
+      </span>
+    </div>
+  );
 }
+
+Timer.propTypes = {
+  startTime: PropTypes.number,
+  className: PropTypes.string
+};
+
+Timer.defaultProps = {
+  startTime: Date.now(),
+  className: ''
+};
 
 export default Timer;
