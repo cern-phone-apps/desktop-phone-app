4621c1e6e929e73745777f6e98f5d53a893433a9
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-10-25 09:33:33 +0200

    Only patch the appImage if this is a linux build

--- docker/configure_appimage.js
+++ docker/configure_appimage.js
@@ -1,9 +1,16 @@
-const system = require("system-commands");
+const system = require('system-commands');
 
 exports.default = async function(context) {
-  console.log("Adding --no-sandox to AppImage");
-  system("docker build docker/");
-  system("docker run -v $(pwd)/dist/:/root/home/ --privileged $(docker images -a | awk '{ print $3}' | awk 'NR==2')");
-  console.log("Done.");
+  const { electronPlatformName } = context;
+  if (electronPlatformName !== 'linux') {
+    return;
+  }
+
+  console.log('Adding --no-sandox to AppImage');
+  system('docker build docker/');
+  system(
+    "docker run -v $(pwd)/dist/:/root/home/ --privileged $(docker images -a | awk '{ print $3}' | awk 'NR==2')"
+  );
+  console.log('Done.');
   return [];
-}
+};
