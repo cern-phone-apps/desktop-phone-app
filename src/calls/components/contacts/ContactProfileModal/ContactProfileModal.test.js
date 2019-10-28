bf6184264fb027007e7016a1cb5254eebe08ef15
Fiser <ruben.garcia.hernando@cern.ch>
2019-10-28 16:31:14 +0100

    Remove unused method from the import

--- src/calls/components/contacts/ContactProfileModal/ContactProfileModal.test.js
+++ src/calls/components/contacts/ContactProfileModal/ContactProfileModal.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, waitForElement } from '@testing-library/react';
+import { render } from '@testing-library/react';
 import ContactProfileModal from './ContactProfileModal';
 
 global.remote = {
