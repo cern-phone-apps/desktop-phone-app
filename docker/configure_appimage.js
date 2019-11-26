1b40caa74cba850be26a39daa166fa2755dd97ba
Rene Fernandez Sanchez <rene.fernandez@cern.ch>
2019-11-26 16:06:16 +0100

    Add prints and use promise on configure_appimage

--- docker/configure_appimage.js
+++ docker/configure_appimage.js
@@ -1,7 +1,6 @@
 const system = require('system-commands');
 
 exports.default = async function(context) {
-
   // TODO Run this only on the linux build
   if (process.env.SET_NO_SANDBOX !== 'true') {
     console.log('Not setting the sandbox mode to false');
@@ -9,10 +8,28 @@ exports.default = async function(context) {
   }
 
   console.log('Adding --no-sandox to AppImage');
-  system('docker build docker/');
+  console.log('Building Docker image');
+  system('docker build docker/')
+    .then(output => {
+      // Log the output
+      console.log(output);
+    })
+    .catch(error => {
+      // An error occurred! Log the error
+      console.error(error);
+    });
+  console.log('Running docker image');
   system(
     "docker run -v $(pwd)/dist/:/root/home/ --privileged $(docker images -a | awk '{ print $3}' | awk 'NR==2')"
-  );
+  )
+    .then(output => {
+      // Log the output
+      console.log(output);
+    })
+    .catch(error => {
+      // An error occurred! Log the error
+      console.error(error);
+    });
   console.log('Done.');
   return [];
 };
